6e7a2d9ea1a6beef5df9e0ba320c3f8a254b782e
mciethan <emaclex@gmail.com>
2023-06-27 22:32:59 -0400

    line spacing tweaks
    addresses #30

--- rules-engine/tests/rules_engine/test_engine.py
+++ rules-engine/tests/rules_engine/test_engine.py
@@ -1,9 +1,11 @@
 from rules_engine import engine
 
+
 def test_hdd():
-    assert engine.hdd(72, 60) == 0 # outside hotter than balance point
-    assert engine.hdd(60, 60) == 0 # outside equal to balance point
-    assert engine.hdd(57, 60) == 3 # outside cooler than balance point
+    assert engine.hdd(72, 60) == 0  # outside hotter than balance point
+    assert engine.hdd(60, 60) == 0  # outside equal to balance point
+    assert engine.hdd(57, 60) == 3  # outside cooler than balance point
+
 
 def test_period_hdd():
     bp = 60
@@ -11,9 +13,10 @@ def test_period_hdd():
     temps_2 = [52, 60, 55]
     temps_3 = [72, 60, 65, 60, 80]
 
-    assert engine.period_hdd(temps_1, bp) == 5 # one day with HDDs
-    assert engine.period_hdd(temps_2, bp) == 13 # two days with HDDs
-    assert engine.period_hdd(temps_3, bp) == 0 # no days with HDDs
+    assert engine.period_hdd(temps_1, bp) == 5  # one day with HDDs
+    assert engine.period_hdd(temps_2, bp) == 13  # two days with HDDs
+    assert engine.period_hdd(temps_3, bp) == 0  # no days with HDDs
+
 
 def test_ua():
     # I pulled the numbers for this test from the spreadsheet
