c21f0b6198e75c72733cb4a867ec9a324ea97b8c
mciethan <emaclex@gmail.com>
2023-06-27 22:32:59 -0400

    line spacing tweaks
    addresses #30

--- rules-engine/src/rules_engine/engine.py
+++ rules-engine/src/rules_engine/engine.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
 
 btu_per_usage = {
-    "gas": 100000 # usage in therms
-    , "oil": 139600 # usage in gallons
-    , "propane": 91333 # usage in gallons
+    "gas": 100000,  # usage in therms
+    "oil": 139600,  # usage in gallons
+    "propane": 91333,  # usage in gallons
 }
+
 
 def hdd(avg_temp: float, balance_point: float) -> float:
     """Calculate the heating degree days on a given day for a given home.
@@ -15,13 +16,14 @@ def hdd(avg_temp: float, balance_point: float) -> float:
     a given home
     """
     diff = balance_point - avg_temp
-    
+
     if diff < 0:
         return 0
     else:
         return diff
-    
-def period_hdd(avg_temps: list, balance_point: float) -> float:                 
+
+
+def period_hdd(avg_temps: list, balance_point: float) -> float:
     """Sum up total heating degree days in a given time period for a given home.
 
     Arguments:
@@ -31,8 +33,14 @@ def period_hdd(avg_temps: list, balance_point: float) -> float:
     """
     return sum([hdd(temp, balance_point) for temp in avg_temps])
 
-def ua(days_in_period: int, daily_heat_usage: float, BTU_per_usage: float, 
-       heat_sys_efficiency: float, period_hdd: int) -> float:
+
+def ua(
+    days_in_period: int,
+    daily_heat_usage: float,
+    BTU_per_usage: float,
+    heat_sys_efficiency: float,
+    period_hdd: int,
+) -> float:
     """Computes the UA coefficient for a given billing period.
 
     Arguments:
@@ -42,23 +50,36 @@ def ua(days_in_period: int, daily_heat_usage: float, BTU_per_usage: float,
     heat_sys_efficiency -- heating system efficiency (decimal between 0 and 1)
     period_hdd -- total number of heating degree days in the given period
     """
-    return days_in_period * daily_heat_usage * BTU_per_usage * \
-        heat_sys_efficiency / (period_hdd * 24)
+    return (
+        days_in_period
+        * daily_heat_usage
+        * BTU_per_usage
+        * heat_sys_efficiency
+        / (period_hdd * 24)
+    )
+
 
 def get_avg_temps(end_dates: list, days_in_bills: list) -> list:
     """Returns a list of lists of average daily temperatures for each period
-    
+
     Arguments:
     end_dates -- list of ending dates for each billing period
     days_in_bills -- lengths in days for the list of billing periods
     """
-    pass # TODO: write this method once we know how we're getting temp data
+    pass  # TODO: write this method once we know how we're getting temp data
 
-def uas(fuel_type: str, balance_point: float, non_heating_usage: float, 
-        heat_sys_efficiency: float, end_dates: list, days_in_bills: list, 
-        usages: list):
+
+def uas(
+    fuel_type: str,
+    balance_point: float,
+    non_heating_usage: float,
+    heat_sys_efficiency: float,
+    end_dates: list,
+    days_in_bills: list,
+    usages: list,
+):
     """Given a list of billing periods, returns a list of UA coefficients
-    
+
     Arguments:
     fuel_type -- heating fuel type in the home. One of "gas", "oil", "propane"
     balance_point -- outdoor temperature (F) above which no heating is required
@@ -73,13 +94,22 @@ def uas(fuel_type: str, balance_point: float, non_heating_usage: float,
     # length, and also that the fuel_type string is valid. Not sure if this is
     # the place to do that checking, or if it will be handled earlier
 
-    avg_daily_heating_usages = [usage/days - non_heating_usage \
-                                for usage, days in zip(usages, days_in_bills)]
-    period_hdds = [period_hdd(temps, balance_point) \
-                   for temps in get_avg_temps(end_dates, days_in_bills)]
+    avg_daily_heating_usages = [
+        usage / days - non_heating_usage for usage, days in zip(usages, days_in_bills)
+    ]
+    period_hdds = [
+        period_hdd(temps, balance_point)
+        for temps in get_avg_temps(end_dates, days_in_bills)
+    ]
     BTU_per_usage = btu_per_usage[fuel_type]
 
-    return [ua(days_in_bills[period], avg_daily_heating_usages[period],
-               BTU_per_usage, heat_sys_efficiency, period_hdds[period])
-               for period in range(len(usages))
+    return [
+        ua(
+            days_in_bills[period],
+            avg_daily_heating_usages[period],
+            BTU_per_usage,
+            heat_sys_efficiency,
+            period_hdds[period],
+        )
+        for period in range(len(usages))
     ]
