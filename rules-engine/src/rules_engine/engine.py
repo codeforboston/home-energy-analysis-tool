b7b5f05085a1e012836c94054917980821abe4e8
mciethan <emaclex@gmail.com>
2023-07-09 10:14:13 -0400

    added test of BP / UA calcs using fake temp data

--- rules-engine/src/rules_engine/engine.py
+++ rules-engine/src/rules_engine/engine.py
@@ -53,47 +53,53 @@ def ua(
     )
 
 
-def get_avg_temps(end_dates: list, days_in_bills: list) -> list:
-    """Returns a list of lists of average daily temperatures for each period
+def average_indoor_temp(
+    tstat_set: float, tstat_setback: float, setback_daily_hrs: float
+) -> float:
+    """Calculates the average indoor temperature.
 
     Arguments:
-    end_dates -- list of ending dates for each billing period
-    days_in_bills -- lengths in days for the list of billing periods
+    tstat_set -- the temp in F at which the home is normally set
+    tstat_setback -- temp in F at which the home is set during off hours
+    setback_daily_hrs -- average # of hours per day the home is at setback temp
     """
-    # TODO: write this method once we know how we're getting temp data
-    return [[]]
+    # again, not sure if we should check for valid values here or whether we can
+    # assume those kinds of checks will be handled at the point of user entry
+    return (
+        (24 - setback_daily_hrs) * tstat_set + setback_daily_hrs * tstat_setback
+    ) / 24
 
 
 def bp_ua_estimates(
     fuel_type: str,
     non_heat_usage: float,
     heat_sys_efficiency: float,
-    end_dates: list,
-    bills_days: list,
+    avg_temps_lists: list,
     usages: list,
     initial_bp: float = 60,
+    bp_sensitivity: float = 2,
 ):
     """Given a list of billing periods, returns an estimate of balance point,
     a list of UA coefficients for each period, and the average UA coefficient.
 
     Arguments:
     fuel_type -- heating fuel type in the home. One of "gas", "oil", "propane"
-    balance_point -- outdoor temperature (F) above which no heating is required
-    in a given home
     non_heat_usage -- estimate of daily non-heating fuel usage
-    end_dates -- list of ending dates for each billing period
-    bills_days -- lengths in days for the list of billing periods
-    usages -- list of fuel usages for each billing period
+    heat_sys_efficiency -- heating system efficiency (decimal between 0 and 1)
+    avg_temps_lists -- list of lists of avg daily temps for each period
+    usages -- list of fuel usages (one for each period, same order as above)
+    initial_bp -- Balance point temperature in degrees F to try at first
+    bp_sensitivity -- Degrees F to add/subtract from balance point when refining
     """
 
     # this implementation assumes that the three list arguments are the same
     # length, and also that the fuel_type string is valid. Not sure if this is
     # the place to do that checking, or if it will be handled earlier
 
+    bills_days = [len(l) for l in avg_temps_lists]
     avg_daily_heating_usages = [
         usage / days - non_heat_usage for usage, days in zip(usages, bills_days)
     ]
-    avg_temps_lists = get_avg_temps(end_dates, bills_days)
     period_hdds = [period_hdd(temps, initial_bp) for temps in avg_temps_lists]
 
     btu_per_usage = {
@@ -115,9 +121,8 @@ def bp_ua_estimates(
     ]
     partial_uas = [ua * period_hdds[period] for period, ua in enumerate(uas)]
     avg_ua = sts.mean(uas)
-    stdev_pct = sts.stdev(uas) / avg_ua
+    stdev_pct = sts.pstdev(uas) / avg_ua
 
-    bp_sensitivity = 2
     directions_to_check = [1, -1]
     bp = initial_bp
 
@@ -126,7 +131,7 @@ def bp_ua_estimates(
         period_hdds_i = [period_hdd(temps, bp_i) for temps in avg_temps_lists]
         uas_i = [pua / period_hdds_i[n] for n, pua in enumerate(partial_uas)]
         avg_ua_i = sts.mean(uas_i)
-        stdev_pct_i = sts.stdev(uas_i) / avg_ua_i
+        stdev_pct_i = sts.pstdev(uas_i) / avg_ua_i
         if stdev_pct_i < stdev_pct:
             bp, uas, avg_ua, stdev_pct = bp_i, uas_i, avg_ua_i, stdev_pct_i
             if len(directions_to_check) == 2:
@@ -134,21 +139,4 @@ def bp_ua_estimates(
         else:
             directions_to_check.pop(0)
 
-    return bp, uas, avg_ua
-
-
-def average_indoor_temp(
-    tstat_set: float, tstat_setback: float, setback_daily_hrs: float
-) -> float:
-    """Calculates the average indoor temperature.
-
-    Arguments:
-    tstat_set -- the temp in F at which the home is normally set
-    tstat_setback -- temp in F at which the home is set during off hours
-    setback_daily_hrs -- average # of hours per day the home is at setback temp
-    """
-    # again, not sure if we should check for valid values here or whether we can
-    # assume those kinds of checks will be handled at the point of user entry
-    return (
-        (24 - setback_daily_hrs) * tstat_set + setback_daily_hrs * tstat_setback
-    ) / 24
+    return bp, uas, avg_ua, stdev_pct
