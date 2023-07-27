bd3141e52234e88788c66cde785602f4fd202488
mciethan <emaclex@gmail.com>
2023-07-26 23:59:12 -0400

    added Home class, made things more object oriented

--- rules-engine/tests/rules_engine/test_engine.py
+++ rules-engine/tests/rules_engine/test_engine.py
@@ -20,16 +20,6 @@ def test_period_hdd():
     assert engine.period_hdd(temps_3, bp) == 0  # no days with HDDs
 
 
-def test_ua():
-    # I pulled the numbers for this test from the spreadsheet
-    bill_days, htg, p_hdd = 32, 6.92, 1015.9
-    btu_per_u = 100000
-    heat_eff = 0.88
-
-    ua_estimate = engine.ua(bill_days, htg, btu_per_u, heat_eff, p_hdd)
-    assert abs(ua_estimate - 799.4) < 0.2
-
-
 def test_average_indoor_temp():
     set_temp = 68
     setback = 62
@@ -41,50 +31,46 @@ def test_average_indoor_temp():
 
 
 def test_bp_ua_estimates():
-    temps = [[28, 29, 30, 29], [32, 35, 35, 38], [41, 43, 42, 42]]
-    usages = [50, 45, 30]
-    billing_periods = [
-        engine.BillingPeriod(temps[i], usages[i], engine.FuelType.GAS, 0.24, 58, 0.88)
-        for i in range(len(usages))
-    ]
-    bp, bills, avg_ua, stdev_pct = engine.bp_ua_estimates(billing_periods)
-    ua_1, ua_2, ua_3 = [bill.ua for bill in bills]
+    home = engine.Home(engine.FuelType.GAS, heat_sys_efficiency = 0.88, initial_balance_point = 58)
 
-    assert bp == 60
+    daily_temps_lists = [[28, 29, 30, 29], [32, 35, 35, 38], [41, 43, 42, 42]]
+    usages = [50, 45, 30]
+    home.initialize_billing_periods(daily_temps_lists, usages, 0.24)
+    home.calculate_balance_point_and_ua()
+    ua_1, ua_2, ua_3 = [bill.ua for bill in home.bills]
+
+    assert home.balance_point == 60
     assert ua_1 == approx(1450.5, abs=1)
     assert ua_2 == approx(1615.3, abs=1)
     assert ua_3 == approx(1479.6, abs=1)
-    assert avg_ua == approx(1515.1, abs=1)
-    assert stdev_pct == approx(0.0474, abs=0.01)
+    assert home.avg_ua == approx(1515.1, abs=1)
+    assert home.stdev_pct == approx(0.0474, abs=0.01)
 
 
 def test_bp_ua_with_outlier():
-    temps = [
+    home = engine.Home(engine.FuelType.GAS, heat_sys_efficiency = 0.88, initial_balance_point = 58)
+    daily_temps_lists = [
         [41.7, 41.6, 32, 25.4],
         [28, 29, 30, 29],
         [32, 35, 35, 38],
         [41, 43, 42, 42],
     ]
     usages = [60, 50, 45, 30]
-    billing_periods = [
-        engine.BillingPeriod(temps[i], usages[i], engine.FuelType.GAS, 0.24, 58, 0.88)
-        for i in range(len(usages))
-    ]
-    bp, bills, avg_ua, stdev_pct = engine.bp_ua_estimates(billing_periods)
-    ua_1, ua_2, ua_3 = [bill.ua for bill in bills]
+    home.initialize_billing_periods(daily_temps_lists, usages, 0.24)
+    home.calculate_balance_point_and_ua()
+    ua_1, ua_2, ua_3 = [bill.ua for bill in home.bills]
 
-    assert bp == 60
+    assert home.balance_point == 60
     assert ua_1 == approx(1450.5, abs=1)
     assert ua_2 == approx(1615.3, abs=1)
     assert ua_3 == approx(1479.6, abs=1)
-    assert avg_ua == approx(1515.1, abs=1)
-    assert stdev_pct == approx(0.0474, abs=0.01)
+    assert home.avg_ua == approx(1515.1, abs=1)
+    assert home.stdev_pct == approx(0.0474, abs=0.01)
 
 
 if __name__ == "__main__":
     test_hdd()
     test_period_hdd()
-    test_ua()
     test_average_indoor_temp()
     test_bp_ua_estimates()
     test_bp_ua_with_outlier()
