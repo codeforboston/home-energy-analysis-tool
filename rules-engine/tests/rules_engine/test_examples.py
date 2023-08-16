fa5d5def91b7e993ec99b092e91139382483c3eb
Alan Pinkert <alan.pinkert@gmail.com>
2023-08-15 21:14:21 -0400

    lint

--- rules-engine/tests/rules_engine/test_examples.py
+++ rules-engine/tests/rules_engine/test_examples.py
@@ -1,7 +1,8 @@
 import json
-from pytest import approx
 import os
 from unittest import TestCase
+
+from pytest import approx
 
 from rules_engine import engine
 
@@ -15,9 +16,13 @@ class TestEngine(TestCase):
     @classmethod
     def setUpClass(cls):
         test_directory = os.path.dirname(os.path.abspath(__file__))
-        cls.example = cls.load_example(test_directory + '/cases/examples/example-1')
+        cls.example = cls.load_example(test_directory + "/cases/examples/example-1")
 
     def test_average_indoor_temp(self):
         data = self.example
-        avg_indoor_temp = engine.average_indoor_temp(data["thermostat_set_point"], data["setback_temp"], data["setback_hours_per_day"])
+        avg_indoor_temp = engine.average_indoor_temp(
+            data["thermostat_set_point"],
+            data["setback_temp"],
+            data["setback_hours_per_day"],
+        )
         assert data["average_indoor_temperature"] == approx(avg_indoor_temp, rel=0.1)
