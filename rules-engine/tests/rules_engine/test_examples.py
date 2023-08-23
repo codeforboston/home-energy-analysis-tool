549b4fe2ead21f0ce9aef80c0f7b6a038f3fcd53
Jonathan Kwan <jkwan2011@users.noreply.github.com>
2023-08-23 02:05:38 +0000

    Parameterized paths for test input

--- rules-engine/tests/rules_engine/test_examples.py
+++ rules-engine/tests/rules_engine/test_examples.py
@@ -1,25 +1,32 @@
 import json
 import os
-from unittest import TestCase
-
+import pathlib
+import pytest
+# import glob
 from pytest import approx
 
 from rules_engine import engine
 
+# Test inputs are provided as separate directory within the "cases/examples" directory
+# Each subdirectory contains a JSON file (named summary.json) which specifies the inputs for the test runner
+#INPUT_DATA = [os.path.dirname(os.path.abspath(__file__)) + "/cases/examples/" + ex for ex in ["example-1"]]
+ROOT_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)), "cases", "examples")
+INPUT_DATA = [o for o in ROOT_DIR.iterdir() if o.is_dir()]
 
-class TestEngine(TestCase):
+
+def test_input_data():
+    print(os.listdir(os.path.dirname(os.path.abspath(__file__)) + "/cases/examples"))
+    assert INPUT_DATA
+
+@pytest.mark.parametrize("file_path", INPUT_DATA)
+class TestEngine:
     @classmethod
     def load_example(cls, folder):
         with open(folder + "/summary.json") as f:
             return json.load(f)
 
-    @classmethod
-    def setUpClass(cls):
-        test_directory = os.path.dirname(os.path.abspath(__file__))
-        cls.example = cls.load_example(test_directory + "/cases/examples/example-1")
-
-    def test_average_indoor_temp(self):
-        data = self.example
+    def test_average_indoor_temp(self, file_path):
+        data = self.load_example(file_path)
         avg_indoor_temp = engine.average_indoor_temp(
             data["thermostat_set_point"],
             data["setback_temp"],
