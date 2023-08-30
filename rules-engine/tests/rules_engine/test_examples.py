56e3d46882b6978c9933872f0cf692924526a7e9
Jonathan Kwan <jkwan2011@users.noreply.github.com>
2023-08-30 00:11:28 +0000

    fixed test_examples.py
    Co-authored-by: Alan Pinkert <alanisaac@users.noreply.github.com>
    Co-authored-by: Ethan McIntosh <mciethan@users.noreply.github.com>
    Co-authored-by: harry <harryzhu626@users.noreply.github.com>
    Co-authored-by: Mihir Gowda <mirgow@users.noreply.github.com>

--- rules-engine/tests/rules_engine/test_examples.py
+++ rules-engine/tests/rules_engine/test_examples.py
@@ -1,32 +1,28 @@
 import json
 import os
 import pathlib
+from typing import Any
+
 import pytest
-# import glob
 from pytest import approx
 
 from rules_engine import engine
 
 # Test inputs are provided as separate directory within the "cases/examples" directory
 # Each subdirectory contains a JSON file (named summary.json) which specifies the inputs for the test runner
-#INPUT_DATA = [os.path.dirname(os.path.abspath(__file__)) + "/cases/examples/" + ex for ex in ["example-1"]]
-ROOT_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)), "cases", "examples")
-INPUT_DATA = [o for o in ROOT_DIR.iterdir() if o.is_dir()]
+ROOT_DIR = pathlib.Path(__file__).parent / "cases" / "examples"
+INPUT_DATA = next(os.walk(ROOT_DIR))[1]
 
 
-def test_input_data():
-    print(os.listdir(os.path.dirname(os.path.abspath(__file__)) + "/cases/examples"))
-    assert INPUT_DATA
+def load_example(folder: str) -> dict[str, Any]:
+    with open(ROOT_DIR / folder / "summary.json") as f:
+        return json.load(f)
+
 
 @pytest.mark.parametrize("file_path", INPUT_DATA)
 class TestEngine:
-    @classmethod
-    def load_example(cls, folder):
-        with open(folder + "/summary.json") as f:
-            return json.load(f)
-
     def test_average_indoor_temp(self, file_path):
-        data = self.load_example(file_path)
+        data = load_example(file_path)
         avg_indoor_temp = engine.average_indoor_temp(
             data["thermostat_set_point"],
             data["setback_temp"],
