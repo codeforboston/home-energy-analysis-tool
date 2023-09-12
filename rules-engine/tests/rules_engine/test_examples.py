f303031f07a6a493ffb19dd03cde427cbea282e4
Alan Pinkert <alan.pinkert@gmail.com>
2023-09-12 19:10:32 -0400

    Load natural gas data for test cases (#69)
    * load natural gas data for test cases
    
    * ignore types for loading from dict
    
    * switch to pydantic, several fixes
    
    * lint

--- rules-engine/tests/rules_engine/test_examples.py
+++ rules-engine/tests/rules_engine/test_examples.py
@@ -1,10 +1,14 @@
+import csv
 import json
 import os
 import pathlib
-from typing import Any
+from datetime import datetime
+from typing import List, Literal, Optional
 
 import pytest
+from pydantic import BaseModel, BeforeValidator
 from pytest import approx
+from typing_extensions import Annotated
 
 from rules_engine import engine
 
@@ -14,18 +18,97 @@ ROOT_DIR = pathlib.Path(__file__).parent / "cases" / "examples"
 INPUT_DATA = next(os.walk(ROOT_DIR))[1]
 
 
-def load_example(folder: str) -> dict[str, Any]:
-    with open(ROOT_DIR / folder / "summary.json") as f:
-        return json.load(f)
-
-
-@pytest.mark.parametrize("file_path", INPUT_DATA)
-class TestEngine:
-    def test_average_indoor_temp(self, file_path):
-        data = load_example(file_path)
-        avg_indoor_temp = engine.average_indoor_temp(
-            data["thermostat_set_point"],
-            data["setback_temp"],
-            data["setback_hours_per_day"],
+def validate_fuel_type(value):
+    try:
+        return engine.FuelType[value]
+    except KeyError as e:
+        raise ValueError(
+            f"Error validating fuel type {e}. Valid choices are: {[x.name for x in engine.FuelType]}"
         )
-        assert data["average_indoor_temperature"] == approx(avg_indoor_temp, rel=0.1)
+
+
+class Summary(BaseModel):
+    local_weather_station: str
+    design_temperature_override: Optional[float]
+    living_area: int
+    fuel_type: Annotated[engine.FuelType, BeforeValidator(validate_fuel_type)]
+    heating_system_efficiency: float
+    other_fuel_usage: Optional[float]
+    other_fuel_usage_override: Optional[float]
+    thermostat_set_point: float
+    setback_temp: float
+    setback_hours_per_day: float
+    estimated_balance_point: float
+    balance_point_sensitivity: float
+    average_indoor_temperature: float
+    difference_between_ti_and_tbp: float
+    design_temperature: float
+    whole_home_ua: int
+    standard_deviation_of_ua: float
+    avg_heat_load: int
+    max_heat_load: int
+
+
+def validate_datetime(value):
+    return datetime.strptime(value, "%m/%d/%Y").date()
+
+
+def validate_inclusion(value):
+    return int(value) if value else None
+
+
+class NaturalGasUsage(BaseModel):
+    start_date: Annotated[datetime, BeforeValidator(validate_datetime)]
+    end_date: Annotated[datetime, BeforeValidator(validate_datetime)]
+    days_in_bill: int
+    usage: int
+    inclusion_override: Annotated[
+        Optional[Literal[-1, 0, 1]], BeforeValidator(validate_inclusion)
+    ]
+    inclusion_code: Annotated[Literal[-1, 0, 1], BeforeValidator(validate_inclusion)]
+    avg_daily_usage: float
+    daily_htg_usage: float
+
+
+class Example(BaseModel):
+    summary: Summary
+    natural_gas_usage: Optional[List[NaturalGasUsage]]
+
+
+def load_summary(folder: str) -> Summary:
+    with open(ROOT_DIR / folder / "summary.json") as f:
+        d = json.load(f)
+        return Summary(**d)
+
+
+def load_natural_gas(folder: str) -> List[NaturalGasUsage]:
+    result = []
+
+    with open(ROOT_DIR / folder / "natural-gas.csv") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            item = NaturalGasUsage(**row)  # type: ignore[arg-type]
+            result.append(item)
+
+    return result
+
+
+@pytest.fixture(scope="module", params=INPUT_DATA)
+def data(request):
+    summary = load_summary(request.param)
+    if summary.fuel_type == engine.FuelType.GAS:
+        natural_gas_usage = load_natural_gas(request.param)
+    else:
+        natural_gas_usage = None
+
+    example = Example(summary=summary, natural_gas_usage=natural_gas_usage)
+    yield example
+
+
+def test_average_indoor_temp(data: Example) -> None:
+    avg_indoor_temp = engine.average_indoor_temp(
+        data.summary.thermostat_set_point,
+        data.summary.setback_temp,
+        data.summary.setback_hours_per_day,
+    )
+    assert data.summary.average_indoor_temperature == approx(avg_indoor_temp, rel=0.1)
