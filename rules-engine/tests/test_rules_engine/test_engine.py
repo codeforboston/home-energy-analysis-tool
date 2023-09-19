44def235a1e78335611a2eba524c7a6c15db79c7
Ethan McIntosh <84473423+mciethan@users.noreply.github.com>
2023-09-19 19:52:59 -0400

    Distinguish billing period seasons (#71)
    * Started incorporating non-heating usage.
    
    Co-authored-by: Alan Pinkert <alanisaac@users.noreply.github.com>
    Co-authored-by: Ethan McIntosh <mciethan@users.noreply.github.com>
    Co-authored-by: harry <harryzhu626@users.noreply.github.com>
    Co-authored-by: claudioolmedo <claudioolmedo@users.noreply.github.com>
    
    * refactor BillingPeriod to avoid circular logic
    
    * refactor tests to have summer billing period
    
    addresses #11
    
    * return type
    
    * declare return types
    
    * isort
    
    * delete old test wrapper to merge main
    
    ---------
    
    Co-authored-by: Jonathan Kwan <jkwan2011@users.noreply.github.com>
    Co-authored-by: Alan Pinkert <alanisaac@users.noreply.github.com>
    Co-authored-by: Ethan McIntosh <mciethan@users.noreply.github.com>
    Co-authored-by: harry <harryzhu626@users.noreply.github.com>
    Co-authored-by: claudioolmedo <claudioolmedo@users.noreply.github.com>

--- rules-engine/tests/test_rules_engine/test_engine.py
+++ rules-engine/tests/test_rules_engine/test_engine.py
@@ -43,11 +43,19 @@ def test_bp_ua_estimates():
         engine.FuelType.GAS, heat_sys_efficiency=0.88, initial_balance_point=58
     )
 
-    daily_temps_lists = [[28, 29, 30, 29], [32, 35, 35, 38], [41, 43, 42, 42]]
-    usages = [50, 45, 30]
-    home.initialize_billing_periods(daily_temps_lists, usages, 0.24)
+    daily_temps_lists = [
+        [28, 29, 30, 29],
+        [32, 35, 35, 38],
+        [41, 43, 42, 42],
+        [72, 71, 70, 69],
+    ]
+    usages = [50, 45, 30, 0.96]
+    inclusion_codes = [1, 1, 1, -1]
+    home.initialize_billing_periods(daily_temps_lists, usages, inclusion_codes)
+    home.calculate_avg_non_heating_usage()
     home.calculate_balance_point_and_ua()
-    ua_1, ua_2, ua_3 = [bill.ua for bill in home.bills]
+
+    ua_1, ua_2, ua_3 = [bill.ua for bill in home.bills_winter]
 
     assert home.balance_point == 60
     assert ua_1 == approx(1450.5, abs=1)
@@ -66,11 +74,14 @@ def test_bp_ua_with_outlier():
         [28, 29, 30, 29],
         [32, 35, 35, 38],
         [41, 43, 42, 42],
+        [72, 71, 70, 69],
     ]
-    usages = [60, 50, 45, 30]
-    home.initialize_billing_periods(daily_temps_lists, usages, 0.24)
+    usages = [60, 50, 45, 30, 0.96]
+    inclusion_codes = [1, 1, 1, 1, -1]
+    home.initialize_billing_periods(daily_temps_lists, usages, inclusion_codes)
+    home.calculate_avg_non_heating_usage()
     home.calculate_balance_point_and_ua()
-    ua_1, ua_2, ua_3 = [bill.ua for bill in home.bills]
+    ua_1, ua_2, ua_3 = [bill.ua for bill in home.bills_winter]
 
     assert home.balance_point == 60
     assert ua_1 == approx(1450.5, abs=1)
