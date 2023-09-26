2c71dad3e478bc043f54ec8b8de5f0f671423d6c
Alan Pinkert <alan.pinkert@gmail.com>
2023-09-26 19:20:04 -0400

    Add example to try out pyodide importing modules (#78)
    * add example to try out pyodide importing classes
    
    * lints

---
+++ rules-engine/src/rules_engine/import_test_1.py
@@ -0,0 +1,12 @@
+"""
+TODO: this module was created to test importing via Pyodide.
+Once that testing is complete, this module should be deleted.
+"""
+
+from . import import_test_2
+
+
+def try_it_out() -> bool:
+    foo = import_test_2.Foo()
+    foo.call_me()
+    return True
---
+++ rules-engine/src/rules_engine/import_test_2.py
@@ -0,0 +1,9 @@
+"""
+TODO: this module was created to test importing via Pyodide.
+Once that testing is complete, this module should be deleted.
+"""
+
+
+class Foo:
+    def call_me(self):
+        print("I was called!")
