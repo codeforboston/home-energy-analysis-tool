f4534153eeaa84410d48ea8248604fa08c15f0b2
axiomizer <nathan.j.jellis@gmail.com>
2023-10-04 19:08:17 -0400

    address review comments

--- design_temp/clean.py
+++ design_temp/clean.py
@@ -6,45 +6,13 @@ from dataclasses import dataclass
 # This script generates design_temp_by_county.csv
 # from the raw text copied from the ENERGY STAR pdf located at:
 # https://www.energystar.gov/ia/partners/bldrs_lenders_raters/downloads/County%20Level%20Design%20Temperature%20Reference%20Guide%20-%202015-06-24.pdf
-
-
-def remove_junk(content):
-    table_title = (
-        "Exhibit 1: Cooling and Heating Design Temperatures by State and County"
-    )
-    content = content.split(table_title)[1]
-    pattern = r"[ ]+ENERGY STAR Certified Homes\nCounty-Level Design Temperature Reference Guide"
-    content, subs = re.subn(pattern, "", content)
-    if subs != 115:
-        raise Exception
-    pattern = (
-        r"State[\s]+County[\s]+1% Cooling Temperature \(°F\)[\s]+"
-        r"99% Heating Temperature \(°F\)[\s]+HDD/CDD Ratio[\s]+"
-        r"Weather Station Selected for Cooling Temperature[\s]+"
-        r"Reference[\s]+Weather Station Selected for Heating Temperature[\s]+Reference"
-    )
-    content, subs = re.subn(pattern, "", content)
-    if subs != 47:
-        raise Exception
-    pattern = (
-        r"1% Cooling State County Temperature\n\(°F\)\n"
-        r"99% Heating HDD/CDD Weather Station Selected for "
-        r"Weather Station Selected for\nTemperature Ratio Cooling "
-        r"Temperature Reference Heating Temperature Reference \(°F\)"
-    )
-    content, subs = re.subn(pattern, "", content)
-    if subs != 69:
-        raise Exception
-    pattern = r"[ ]+Page [0-9]+ of 116"
-    content, subs = re.subn(pattern, "", content)
-    if subs != 116:
-        raise Exception("subs: {}".format(subs))
-    return content
+# To copy the text from the PDF you can open it in chrome and select the 'print' option
+# to save it as a PDF that will allow copying
 
 
 # Number of counties (according to PDF) for each of the 50 states and for DC
 # Same order as PDF
-regions = {
+REGIONS = {
     "Alaska": 28,
     "Alabama": 67,
     "Arkansas": 75,
@@ -99,6 +67,44 @@ regions = {
 }
 
 
+def remove_junk(content):
+    # remove everything before the table
+    table_title = (
+        "Exhibit 1: Cooling and Heating Design Temperatures by State and County"
+    )
+    content = content.split(table_title)[1]
+    # remove the title that appears at the top of each page
+    pattern = r"[ ]+ENERGY STAR Certified Homes\nCounty-Level Design Temperature Reference Guide"
+    content, subs = re.subn(pattern, "", content)
+    if subs != 115:
+        raise Exception("Wrong number of substitutions: {}".format(subs))
+    # remove the column labels from each page (they fit one of two patterns)
+    pattern = (
+        r"State[\s]+County[\s]+1% Cooling Temperature \(°F\)[\s]+"
+        r"99% Heating Temperature \(°F\)[\s]+HDD/CDD Ratio[\s]+"
+        r"Weather Station Selected for Cooling Temperature[\s]+"
+        r"Reference[\s]+Weather Station Selected for Heating Temperature[\s]+Reference"
+    )
+    content, subs = re.subn(pattern, "", content)
+    if subs != 47:
+        raise Exception("Wrong number of substitutions: {}".format(subs))
+    pattern = (
+        r"1% Cooling State County Temperature\n\(°F\)\n"
+        r"99% Heating HDD/CDD Weather Station Selected for "
+        r"Weather Station Selected for\nTemperature Ratio Cooling "
+        r"Temperature Reference Heating Temperature Reference \(°F\)"
+    )
+    content, subs = re.subn(pattern, "", content)
+    if subs != 69:
+        raise Exception("Wrong number of substitutions: {}".format(subs))
+    # remove the page numbers that appear at the bottom of each page
+    pattern = r"[ ]+Page [0-9]+ of 116"
+    content, subs = re.subn(pattern, "", content)
+    if subs != 116:
+        raise Exception("Wrong number of substitutions: {}".format(subs))
+    return content
+
+
 @dataclass
 class DataRow:
     state: str
@@ -110,14 +116,14 @@ def parse(content: str):
     # find where each region starts
     # (it starts at the first instance of the region name appearing twice)
     matches = []
-    for r in regions:
+    for r in REGIONS:
         match = re.search(r + r"[\s]*" + r, content)
         if match is None:
             raise Exception("Could not find data for region: {}".format(r))
         matches.append(match)
     # get table rows for each region
     rows = []
-    for i, r in enumerate(regions):
+    for i, r in enumerate(REGIONS):
         # get the substring of content for a particular region
         end_index = matches[i + 1].start() if i + 1 < len(matches) else len(content)
         r_content = r + content[matches[i].end() : end_index]
@@ -126,18 +132,19 @@ def parse(content: str):
         pattern = r"({})[\s]+([A-Za-z \-\.\']+?)[\s]+[0-9]+[\s]+([\-0-9]+)[\s]+[0-9\.]+"
         pattern = pattern.format(r)
         row_matches = re.findall(pattern, r_content)
-        if len(row_matches) != regions[r]:
+        if len(row_matches) != REGIONS[r]:
             s = "found {} rows for {}, expected {}"
-            raise Exception(s.format(len(row_matches), r, regions[r]))
+            raise Exception(s.format(len(row_matches), r, REGIONS[r]))
         rows += [DataRow(*m) for m in row_matches]
-    if len(rows) != sum(regions.values()):
+    if len(rows) != sum(REGIONS.values()):
         s = "found {} rows, expected {}"
-        raise Exception(s.format(len(rows), sum(regions.values())))
+        raise Exception(s.format(len(rows), sum(REGIONS.values())))
     return rows
 
 
-with open("pdf_rawcopy.txt") as f:
-    content = f.read()
+if __name__ == "__main__":
+    with open("pdf_rawcopy.txt") as f:
+        content = f.read()
     content = remove_junk(content)
     rows = parse(content)
     with open("design_temp_by_county.csv", "w", newline="") as csvfile:
