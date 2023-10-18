b36d8d4f123dd0479942c8a6c8cf902774f5491e
Thad Kerosky <contact@thadk.net>
2023-10-18 01:19:06 +0000

    Form trial ensemble programming
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: Gavin Kimball <Gkimbo@users.noreply.github.com>
    Co-authored-by: Leopardfoot <Leopardfoot@users.noreply.github.com>
    Co-authored-by: Jeff Korenstein <jkoren@users.noreply.github.com>

--- heat-stack/app/routes/_marketing+/index.tsx
+++ heat-stack/app/routes/_marketing+/index.tsx
@@ -54,30 +54,6 @@ export default function Index() {
 						</div>
 					</div>
 				</div>
-
-				<div className="mx-auto mt-8 max-w-7xl px-4 py-2 sm:px-6 lg:px-8">
-					<div className="flex flex-wrap justify-center gap-8 rounded-3xl bg-slate-100 py-4 dark:bg-slate-200">
-						<TooltipProvider>
-							{logos.map(img => (
-								<Tooltip key={img.href}>
-									<TooltipTrigger asChild>
-										<a
-											href={img.href}
-											className="flex h-16 w-32 justify-center p-1 grayscale transition hover:grayscale-0 focus:grayscale-0"
-										>
-											<img
-												alt={img.alt}
-												src={img.src}
-												className="object-contain"
-											/>
-										</a>
-									</TooltipTrigger>
-									<TooltipContent>{img.alt}</TooltipContent>
-								</Tooltip>
-							))}
-						</TooltipProvider>
-					</div>
-				</div>
 			</div>
 		</main>
 	)
