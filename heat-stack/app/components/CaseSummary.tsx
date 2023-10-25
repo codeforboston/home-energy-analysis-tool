6954215614552eb034c8bcc90c9ec497aa4c912e
jkoren <j.korenstein@gmail.com>
2023-10-24 20:19:05 -0400

    clear comment

--- heat-stack/app/components/CaseSummary.tsx
+++ heat-stack/app/components/CaseSummary.tsx
@@ -21,29 +21,3 @@ export function CaseSummary() {
 		</div>
 	)
 }
-
-// export function CaseSummary() {
-// 	}
-
-// 	return (
-// 		<div>
-// 			{/* <span>time/temp first item: {time}/{temp}</span> */}
-// 			<h1>Winnie Carroll's Case Summary</h1>
-// 			<h3>179 Broadway, Somerville, MA 02145, United States</h3>
-// 			<ul>
-// 				<li>Summary of heat load analysis outputs</li>
-// 				<ul>
-// 					<li>Avg heat load at design temp: 58,582 BTU/h</li>
-// 					<li>Average indoor temperature, Ti: 60.0 F</li>
-// 					<li>Standard deviation of UA: 5.08%</li>
-// 				</ul>
-// 				<br />
-// 				<ul>
-// 					<li>Max heat load at design temp: 69,783 BTU/h</li>
-// 					<li>Design temperature: 8.4 F</li>
-// 					<li>Whole-home UA: 1057 BTU/h-F</li>
-// 				</ul>
-// 			</ul>
-// 		</div>
-// 	)
-// }
