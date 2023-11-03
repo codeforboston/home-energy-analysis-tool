c37d8745673551ba732100e4d935a659ff7d2268
jkoren <j.korenstein@gmail.com>
2023-11-02 22:01:23 -0400

    created cards & styling

--- heat-stack/app/components/CaseSummary.tsx
+++ heat-stack/app/components/CaseSummary.tsx
@@ -11,116 +11,139 @@ import {
 export function CaseSummary() {
 	const data = [
 		{
-			name: 'Page A',
-			uv: 4000,
-			pv: 2400,
-			amt: 2400,
+			name: '58 F',
+			uv: 15,
+			amt: 9,
 		},
 		{
-			name: 'Page B',
-			uv: 3000,
-			pv: 1398,
-			amt: 2210,
+			name: '59 F',
+			uv: 11.3,
+			amt: 8.3,
 		},
 		{
-			name: 'Page C',
-			uv: 2000,
-			pv: 9800,
-			amt: 2290,
+			name: '60 F',
+			uv: 7.9,
+			amt: 8.6,
 		},
 		{
-			name: 'Page D',
-			uv: 2780,
-			pv: 3908,
-			amt: 2000,
+			name: '61 F',
+			uv: 6.4,
+			amt: 7.5,
 		},
 		{
-			name: 'Page E',
-			uv: 1890,
-			pv: 4800,
-			amt: 2181,
+			name: '62 F',
+			uv: 7.1,
+			amt: 8.2,
 		},
 		{
-			name: 'Page F',
-			uv: 2390,
-			pv: 3800,
-			amt: 2500,
-		},
-		{
-			name: 'Page G',
-			uv: 3490,
-			pv: 4300,
-			amt: 2100,
+			name: '63 F',
+			uv: 13.12,
+			amt: 7.89,
 		},
 	]
 
 	return (
 		<main className="main-container">
 			<div>
-				<b>Back to website</b>
-				<br />
-				<h3>Winnie Carroll’s Case Summary</h3>
-				<h5>179 Broadway, Somerville, MA 02145</h5>
+				<div className="navigation">Back to website</div>
+
+				<div className="header-cards">
+					<div className="card-inner">
+						<h5 className="text-h5">Winnie Carroll’s Case Summary</h5>
+					</div>
+
+					<div className="card-inner">179 Broadway, Somerville, MA 02145</div>
+				</div>
 				<div className="main-title">
-					<h3>Summary of heat load analysis outputs</h3>
+					<h5 className="text-center text-h5">
+						Summary of heat load analysis outputs
+					</h5>
 				</div>
 			</div>
+
 			<div className="main-cards">
 				<div className="card-inner">
-					<img
-						src="https://heat-tool.s3.amazonaws.com/graphic1.png"
-						alt="grapic"
-					/>
+					<div className="card-title">
+						Avg heat load at design temp
+						<br />
+						<br />
+						<b className="card-statistic">58,582 BTU/h</b>
+					</div>
 				</div>
 
 				<div className="card-inner">
-					<img
-						src="https://heat-tool.s3.amazonaws.com/graphic2.png"
-						alt="grapic"
-					/>
+					<div className="card-title">
+						Average indoor temp
+						<br />
+						<br />
+						<b className="card-statistic">60.0 F</b>
+					</div>
 				</div>
 
 				<div className="card-inner">
-					<img
-						src="https://heat-tool.s3.amazonaws.com/graphic3.png"
-						alt="grapic"
-					/>
+					<div className="card-title">
+						Standard deviation of UA
+						<br />
+						<br />
+						<b className="card-statistic">5.08%</b>
+					</div>
 				</div>
 
 				<div className="card-inner">
-					<img
-						src="https://heat-tool.s3.amazonaws.com/graphic4.png"
-						alt="grapic"
-					/>
+					<div>
+						Balance point
+						<LineChart
+							width={250}
+							height={150}
+							data={data}
+							margin={{
+								top: 5,
+								right: 30,
+								left: 20,
+								bottom: 5,
+							}}
+						>
+							<CartesianGrid strokeDasharray="3 3" />
+							<XAxis dataKey="name" />
+							<YAxis />
+							<Tooltip />
+							<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+						</LineChart>
+					</div>
+				</div>
+
+				<div className="card-inner">
+					<div className="card-title">
+						Max heat load at design temp
+						<br />
+						<br />
+						<b className="card-statistic">69,793 BTU/h</b>
+					</div>
+				</div>
+
+				<div className="card-inner">
+					<div className="card-title">
+						Design temp
+						<br />
+						<br />
+						<b className="card-statistic">8.4 F</b>
+					</div>
+				</div>
+
+				<div className="card-inner">
+					<div className="card-title">
+						Whole home UA
+						<br />
+						<br />
+						<b className="card-statistic">1057 BTU/h-F</b>
+					</div>
 				</div>
 			</div>
+
 			<div className="main-cards">
-				<div className="card-inner">
-					<LineChart
-						width={250}
-						height={150}
-						data={data}
-						margin={{
-							top: 5,
-							right: 30,
-							left: 20,
-							bottom: 5,
-						}}
-					>
-						<CartesianGrid strokeDasharray="3 3" />
-						<XAxis dataKey="name" />
-						<YAxis />
-						<Tooltip />
-						<Legend />
-						<Line
-							type="monotone"
-							dataKey="pv"
-							stroke="#8884d8"
-							activeDot={{ r: 8 }}
-						/>
-						<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-					</LineChart>
-				</div>
+				<div className="card-inner"></div>
+
+				<div className="card-inner"></div>
 			</div>
 		</main>
 	)
