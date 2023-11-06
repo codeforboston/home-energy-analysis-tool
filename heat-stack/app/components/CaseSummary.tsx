b15ead48a806fceeb85bc25cc669a7af924f73bd
jkoren <j.korenstein@gmail.com>
2023-11-06 16:13:10 -0500

    adjusted mock up to reflect 11/5/23 UA revision

--- heat-stack/app/components/CaseSummary.tsx
+++ heat-stack/app/components/CaseSummary.tsx
@@ -1,6 +1,28 @@
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
 
 export function CaseSummary() {
+	const name = 'Pietro Schirano'
+	const street = '567 Pine Avenue Apt 21'
+	const city = 'Rivertown'
+	const state = 'MA'
+	const zip = '02856'
+	const country = 'United States of America'
+	const livingArea = '3000'
+	const designTemperature = '63'
+	const designTemperatureOverride = '65'
+	const fuelType = 'Natural Gas'
+	const heatingSystemEfficiency = '75'
+	const setPoint = '70'
+	const setbackTemperature = '65'
+	const setbackTime = '7'
+	const fileName = '20200930_Eversource.csv'
+	const averageIndoorTemperature = '63.5'
+	const dailyOtherUsage = '1.07'
+	const balancePoint = '60.5'
+	const numPeriodsIncluded = '30 / 36'
+	const standardDevationUA = '5.52'
+	const wholeHomeUA = '1,112'
+
 	const data = [
 		{
 			name: '58 F',
@@ -37,98 +59,123 @@ export function CaseSummary() {
 	return (
 		<main className="main-container">
 			<div>
-				<div className="navigation">Back to website</div>
+				<div className="page-title"> Case Summary</div>
 
-				<div className="header-cards">
+				<div className="section-title"> Home Information </div>
+				<div className="two-cards">
 					<div className="card-inner">
-						<h5 className="text-h5">Winnie Carroll’s Case Summary</h5>
+						<div className="item-title">
+							Resident / Client
+							<br />
+							<div className="item">{name}</div> <br />
+							Address
+							<div className="item">{street}</div>
+							<div className="item">
+								{city}, {state}, {zip}
+							</div>
+							<div className="item">{country}</div> <br />
+							Living Area (sf)
+							<div className="item">{livingArea}</div>
+						</div>
 					</div>
 
-					<div className="card-inner">179 Broadway, Somerville, MA 02145</div>
-				</div>
-				<div className="main-title">
-					<h5 className="text-center text-h5">
-						Summary of heat load analysis outputs
-					</h5>
-				</div>
-			</div>
-
-			<div className="main-cards">
-				<div className="card-inner">
-					<div className="card-title">
-						Avg heat load at design temp
-						<br />
-						<br />
-						<b className="card-statistic">58,582 BTU/h</b>
-					</div>
-				</div>
-
-				<div className="card-inner">
-					<div className="card-title">
-						Average indoor temp
-						<br />
-						<br />
-						<b className="card-statistic">60.0 F</b>
+					<div className="card-inner">
+						<div className="item-title">
+							Design Temperature (°F) <br />
+							<div className="item">{designTemperature}</div> <br />
+							<div className="item">Override: {designTemperatureOverride}</div>
+						</div>
 					</div>
 				</div>
 
-				<div className="card-inner">
-					<div className="card-title">
-						Standard deviation of UA
-						<br />
-						<br />
-						<b className="card-statistic">5.08%</b>
+				<hr />
+
+				<div className="section-title"> Current Heating System </div>
+				<div className="two-cards">
+					<div className="card-inner">
+						<div className="item-title">
+							Fuel Type
+							<br />
+							<div className="item">{fuelType}</div> <br />
+							Heating System Efficiency (%)
+							<br />
+							<div className="item">{heatingSystemEfficiency}</div> <br />
+						</div>
+					</div>
+
+					<div className="card-inner">
+						<div className="item-title">
+							Set Point (°F) <br />
+							<div className="item">{setPoint}</div> <br />
+							Setback Temperature (°F)
+							<br />
+							<div className="item">{setbackTemperature}</div> <br />
+							Setback Time (h)
+							<div className="item">{setbackTime}</div>
+						</div>
 					</div>
 				</div>
 
-				<div className="card-inner">
-					<div>
-						Balance point
-						<LineChart
-							width={250}
-							height={150}
-							data={data}
-							margin={{
-								top: 5,
-								right: 30,
-								left: 20,
-								bottom: 5,
-							}}
-						>
-							<CartesianGrid strokeDasharray="3 3" />
-							<XAxis dataKey="name" />
-							<YAxis />
-							<Tooltip />
-							<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-						</LineChart>
+				<hr />
+
+				<div className="section-title">Energy Use History</div>
+				<div className="item-title">
+					Data Source
+					<br />
+					<div className="item">{fileName}</div> <br />
+				</div>
+				<div className="item-title">Analysis</div>
+				<div className="three-cards">
+					<div className="card-inner">
+						<div className="item-title-small">
+							Average Indoor Temperature (°F) <br />
+							<div className="item">{averageIndoorTemperature}</div> <br />
+							Daily Other Usage <br />
+							<div className="item">{dailyOtherUsage}</div> <br />
+						</div>
+					</div>
+
+					<div className="card-inner">
+						<div className="item-title-small">
+							Balance Point (°F) <br />
+							<div className="item">{balancePoint}</div> <br />
+							No. of Periods Included <br />
+							<div className="item">{numPeriodsIncluded}</div> <br />
+						</div>
+					</div>
+
+					<div className="card-inner">
+						<div className="item-title-small">
+							Standard Deviation of UA (%) <br />
+							<div className="item">{standardDevationUA}</div> <br />
+							Whole-home UA (BTU/h-F) <br />
+							<div className="item">{wholeHomeUA}</div> <br />
+						</div>
 					</div>
 				</div>
 
-				<div className="card-inner">
-					<div className="card-title">
-						Max heat load at design temp
-						<br />
-						<br />
-						<b className="card-statistic">69,793 BTU/h</b>
-					</div>
-				</div>
+				<hr />
 
-				<div className="card-inner">
-					<div className="card-title">
-						Design temp
-						<br />
-						<br />
-						<b className="card-statistic">8.4 F</b>
-					</div>
-				</div>
-
-				<div className="card-inner">
-					<div className="card-title">
-						Whole home UA
-						<br />
-						<br />
-						<b className="card-statistic">1057 BTU/h-F</b>
-					</div>
+				<div className="section-title">Graphs</div>
+				<div className="item-title">
+					Balance point
+					<LineChart
+						width={750}
+						height={450}
+						data={data}
+						margin={{
+							top: 5,
+							right: 30,
+							left: 20,
+							bottom: 5,
+						}}
+					>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name" />
+						<YAxis />
+						<Tooltip />
+						<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+					</LineChart>
 				</div>
 			</div>
 		</main>
