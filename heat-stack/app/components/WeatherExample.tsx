9dae184628d763144fa47627c949ea53347b0716
Thad Kerosky <contact@thadk.net>
2023-11-22 11:54:36 -0500

    fix linter

--- heat-stack/app/components/WeatherExample.tsx
+++ heat-stack/app/components/WeatherExample.tsx
@@ -1,22 +1,23 @@
-import { loader } from "#app/root.tsx";
-import test from "@playwright/test";
-import { json } from "@remix-run/node"; // or cloudflare/deno
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData } from '@remix-run/react'
+import { type loader } from '#app/root.tsx'
+// import test from "@playwright/test";
+// import { json } from "@remix-run/node"; // or cloudflare/deno
 
 // weather `loader` is in root.tsx
 
 export function WeatherExample() {
+	const one_loader = useLoaderData<typeof loader>()
+	if (!one_loader.weather) {
+		return <div>Loading weather...</div>
+	}
+	const time: string = one_loader.weather.daily?.time[0]
+	const temp: number = one_loader.weather.daily?.temperature_2m_max[0]
 
-  const one_loader = useLoaderData<typeof loader>();
-  if (!one_loader.weather) {
-    return <div>Loading weather...</div>;
-  }
-  const time: string = one_loader.weather.daily?.time[0]
-  const temp: number = one_loader.weather.daily?.temperature_2m_max[0]
-  
-  return (
-    <div>
-      <span>time/temp first item: {time}/{temp}</span>
-    </div>
-  );
+	return (
+		<div>
+			<span>
+				time/temp first item: {time}/{temp}
+			</span>
+		</div>
+	)
 }
