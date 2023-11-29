1f36ec5e1dfc5803ddc4072c0637212d5dff7fa9
Alan Pinkert <alan.pinkert@gmail.com>
2023-11-28 19:56:41 -0500

    Add pydantic and user input models (#77)
    * adding pydantic dependency
    
    * Added user input models.
    
    Co-authored-by: Erika Nesse <erika-n@users.noreply.github.com>
    Co-authored-by: Alan Pinkert <alanisaac@users.noreply.github.com>
    Co-authored-by: Debajyoti Debnath <debajyotid2@users.noreply.github.com>
    Co-authored-by: harry <harryzhu626@users.noreply.github.com>
    
    * Added SummaryOutput class.
    Refactored docstrings to 72 character maximum.
    Refactored if-else statement.
    
    Co-authored-by: Erika Nesse <erika-n@users.noreply.github.com>
    Co-authored-by: Nate <axiomizer@users.noreply.github.com>
    Co-authored-by: harry <harryzhu626@users.noreply.github.com>
    Co-authored-by: Debajyoti Debnath <debajyotid2@users.noreply.github.com>
    Co-authored-by: Jeff Korenstein <jkoren@users.noreply.github.com>
    
    * black
    
    * Moved Pydantic models into split folder
    
    Co-authored-by: Erika Nesse <erika-n@users.noreply.github.com>
    Co-authored-by: harry <harryzhu626@users.noreply.github.com>
    Co-authored-by: Debajyoti Debnath <debajyotid2@users.noreply.github.com>
    Co-authored-by: Jonathan Kwan <jkwan2011@users.noreply.github.com>
    Co-authored-by: Nate <axiomizer@users.noreply.github.com>
    
    * updated pydantic models
    
    * Refactor Home class in engine.py.
    
    * changes from this session
    
    * cleaned up code from halloween!
    
    * added SummaryInput to home
    
    * Fixed black
    
    Co-authored-by: harry <harryzhu626@users.noreply.github.com>
    Co-authored-by: Erika Nesse <erika-n@users.noreply.github.com>
    Co-authored-by: Debajyoti Debnath <debajyotid2@users.noreply.github.com>
    Co-authored-by: KKaempen <KKaempen@users.noreply.github.com>
    Co-authored-by: Alan Pinkert <alanisaac@users.noreply.github.com>
    
    * fix mypy
    
    * added entry point functions, inclusion code conversion, and weather data class
    
    Co-authored-by: Jonathan Kwan <jkwan2011@users.noreply.github.com>
    Co-authored-by: Debajyoti Debnath <debajyotid2@users.noreply.github.com>
    Co-authored-by: Erika Nesse <erika-n@users.noreply.github.com>
    
    * updated tests based on new framework
    
    Co-authored-by: harry <harryzhu626@users.noreply.github.com>
    
    * fix lints
    
    * fixed datetime parsing
    
    Co-authored-by: Debajyoti Debnath <debajyotid2@users.noreply.github.com>
    Co-authored-by: Jonathan Kwan <jkwan2011@users.noreply.github.com>
    
    * mypy fix
    
    ---------
    
    Co-authored-by: Jonathan Kwan <jkwan2011@users.noreply.github.com>
    Co-authored-by: Erika Nesse <erika-n@users.noreply.github.com>
    Co-authored-by: Alan Pinkert <alanisaac@users.noreply.github.com>
    Co-authored-by: Debajyoti Debnath <debajyotid2@users.noreply.github.com>
    Co-authored-by: harry <harryzhu626@users.noreply.github.com>
    Co-authored-by: Nate <axiomizer@users.noreply.github.com>
    Co-authored-by: Jeff Korenstein <jkoren@users.noreply.github.com>
    Co-authored-by: KKaempen <KKaempen@users.noreply.github.com>

---
+++ rules-engine/src/rules_engine/pydantic_models.py
@@ -0,0 +1,123 @@
+"""
+Data models for input and output data in the rules engine.
+"""
+
+from datetime import date
+from enum import Enum
+from typing import Annotated, Any, List, Optional
+
+from pydantic import BaseModel, BeforeValidator, Field
+
+
+class AnalysisType(Enum):
+    DO_NOT_INCLUDE = 0
+    INCLUDE = 1
+    INCLUDE_IN_OTHER_ANALYSIS = -1
+
+
+class FuelType(Enum):
+    """Enum for fuel types. Values are BTU per usage"""
+
+    GAS = 100000
+    OIL = 139600
+    PROPANE = 91333
+
+
+def validate_fuel_type(value: Any) -> FuelType:
+    if isinstance(value, FuelType):
+        return value
+
+    try:
+        return FuelType[value]
+    except KeyError as e:
+        raise ValueError(
+            f"Error validating fuel type {e}. Valid choices are: {[x.name for x in FuelType]}"
+        )
+
+
+class SummaryInput(BaseModel):
+    """From Summary Tab"""
+
+    # design_temperature_override: float
+    living_area: float = Field(description="Summary!B10")
+    fuel_type: Annotated[FuelType, BeforeValidator(validate_fuel_type)] = Field(
+        description="Summary!B11"
+    )
+    heating_system_efficiency: float = Field(description="Summary!B12")
+    thermostat_set_point: float = Field(description="Summary!B17")
+    setback_temperature: Optional[float] = Field(description="Summary!B18")
+    setback_hours_per_day: Optional[float] = Field(description="Summary!B19")
+
+
+class DhwInput(BaseModel):
+    """From DHW (Domestic Hot Water) Tab"""
+
+    number_of_occupants: int = Field(description="DHW!B4")
+    estimated_water_heating_efficiency: float = Field(description="DHW!B5")
+    stand_by_losses: float = Field(description="DHW!B6")
+
+
+class OilPropaneBillingInput(BaseModel):
+    """From Oil-Propane tab"""
+
+    period_end_date: date = Field(description="Oil-Propane!B")
+    gallons: float = Field(description="Oil-Propane!C")
+    inclusion_override: Optional[bool] = Field(description="Oil-Propane!F")
+
+
+class NaturalGasBillingRecordInput(BaseModel):
+    """From Natural Gas tab. A single row of the Billing input table."""
+
+    period_start_date: date = Field(description="Natural Gas!A")
+    period_end_date: date = Field(description="Natural Gas!B")
+    usage_therms: float = Field(description="Natural Gas!D")
+    inclusion_override: Optional[AnalysisType] = Field(description="Natural Gas!E")
+
+
+class NaturalGasBillingInput(BaseModel):
+    """From Natural Gas tab. Container for holding all rows of the billing input table."""
+
+    records: List[NaturalGasBillingRecordInput]
+
+
+class TemperatureInput(BaseModel):
+    dates: List[date]
+    temperatures: List[float]
+
+
+class SummaryOutput(BaseModel):
+    """From Summary tab"""
+
+    estimated_balance_point: float = Field(
+        description="Summary!B20"
+    )  # This is hand-calculated in the spreadsheet
+    other_fuel_usage: float = Field(description="Summary!B15")
+    average_indoor_temperature: float = Field(description="Summary!B24")
+    difference_between_ti_and_tbp: float = Field(description="Summary!B25")
+    design_temperature: float = Field(description="Summary!B26")
+    whole_home_heat_loss_rate: float = Field(
+        description="Summary!B27"
+    )  # UA = heat loss rate
+    standard_deviation_of_heat_loss_rate: float = Field(description="Summary!B28")
+    average_heat_load: float = Field(description="Summary!B29")
+    maximum_heat_load: float = Field(description="Summary!B30")
+
+
+class BalancePointGraphRow(BaseModel):
+    """From Summary page"""
+
+    balance_pt: float = Field(description="Summary!G33:35")
+    ua: float = Field(description="Summary!H33:35")
+    change_in_ua: float = Field(description="Summary!I33:35")
+    pct_change: float = Field(description="Summary!J33:35")
+    std_dev: float = Field(description="Summary!K33:35")
+
+
+class BalancePointGraph(BaseModel):
+    """From Summary page"""
+
+    records: List[BalancePointGraphRow]
+
+
+class Constants:
+    balance_point_sensitivity: float = 0.5
