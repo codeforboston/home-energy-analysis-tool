2653249755fad69612c978dcc64e4101efaf8130
jkoren <j.korenstein@gmail.com>
2024-01-02 22:37:27 -0500

    attempt 1

---
+++ heat-stack/models/Home.tsx
@@ -0,0 +1,12 @@
+export type HomeModel = {
+	first_name: string
+	last_name: string
+	livingArea: string
+	designTemperature: string
+	designTemperatureOverride: string
+	fuelType: string
+	heatingSystemEfficiency: string
+	setPoint: string
+	setbackTemperature: string
+	setbackTime: string
+}
