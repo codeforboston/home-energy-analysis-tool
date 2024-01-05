decee55a9bac523246669de59b5d8fc912320326
clayton-schneider <schneiderc2016@gmail.com>
2024-01-05 11:55:15 -0500

    added baseUrl, swapped styles to tw, swapped to shad

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/HomeInformation.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/HomeInformation.tsx
@@ -1,63 +1,77 @@
 import { Form } from '@remix-run/react'
-import { Input } from '../../input.tsx'
-import { Label } from '../../label.tsx'
+import { Input } from '#/app/components/ui/input.tsx'
+import { Label } from '#/app/components/ui/label.tsx'
+import { Button } from '#/app/components/ui/button.tsx'
 
 export function HomeInformation() {
-	const name = 'Pietro Schirano'
-	const street = '567 Pine Avenue Apt 21'
-	const city = 'Rivertown'
-	const state = 'MA'
-	const zip = '02856'
-	const country = 'United States of America'
-	const livingArea = '3,000'
 	const designTemperature = '63'
 	const designTemperatureOverride = '65'
 	return (
-		<div className="section-title">
-			Home Information
-			<hr />
-			<div className="flex flex-row">
-				<div className="basis-1/2">
-					<div className="item-title">
-						Resident / Client
-						<br />
-						<div className="item-big">{name}</div> <br />
-						Address
-						<div className="item-big">{street}</div>
-						<div className="item-big">
-							{city}, {state}, {zip}
-						</div>
-						<div className="item-big">{country}</div> <br />
-						Living Area (sf)
-						<div className="item-big">{livingArea}</div>
+		<div>
+			<h2 className='text-5xl font-extrabold tracking-wide'>Home Information</h2>
+
+			<div className='mt-10'>
+				<h6 className='text-zinc-950 text-2xl font-semibold'>Resident/Client</h6>
+
+				<div className='mt-4 flex space-x-4'>
+					<div>
+						<Label htmlFor="firstName">First Name</Label>
+						<Input id="firstName" type="text" placeholder="Bob" />
+					</div>
+					<div>
+						<Label htmlFor="lastName">Last Name</Label>
+						<Input id="lastName" type="text" placeholder="Barker" />
 					</div>
 				</div>
 
-				<div className="basis-1/2">
-					<div className="item-title">
-						Design Temperature (°F) <br />
-						<div className="item-big">{designTemperature}</div> <br />
-						<Form
-							method="GET"
-							action="/users"
-							className="flex flex-wrap items-center justify-center gap-2"
-							onChange={e => true}
-						>
-							<div className="flex-1">
-								<Label className="item-title" htmlFor="override">
-									Override
-								</Label>
-								<Input
-									type="text"
-									name="override"
-									id="override"
-									defaultValue={designTemperatureOverride}
-									className="w-full"
-								/>
+			</div>
+
+
+			<div className='mt-9'>
+				<h6 className='text-zinc-950 text-2xl font-semibold'>Address</h6>
+
+				<div className='mt-4 flex space-x-4'>
+					<div>
+						<Label htmlFor="address">Street adress</Label>
+						<Input id="address" type="text" placeholder="76 Framingham Ave." />
+						<Input id="adressTwo" type="text" placeholder="Apt 256" />
+
+						<div className='mt-4 flex'>
+							<div>
+								<Label htmlFor="city">City/Town</Label>
+								<Input id="city" type="text" placeholder="Boston" />
 							</div>
-						</Form>
+							<div>
+								<Label htmlFor="state">State</Label>
+								<Input id="state" type="text" placeholder="MA" />
+							</div>
+							<div>
+								<Label htmlFor="zipcode">Zipcode</Label>
+								<Input id="zipcode" type="text" placeholder="02856" />
+							</div>
+						</div>
 					</div>
 				</div>
+			</div>
+
+
+			<div className='mt-9'>
+				<h6>
+					<Label className='text-zinc-950 text-2xl font-semibold' htmlFor="livingArea">
+						Living Area (sf)
+					</Label>
+				</h6>
+
+				<div className='mt-4'>
+					<div>
+						<Input id="livingArea" type="number" placeholder="3000" />
+						<p className='mt-2 text-sm text-slate-500'>The home's above-grade, conditioned space</p>
+					</div>
+				</div>
+			</div>
+
+			<div>
+				<Button>Next ={'>'}</Button>
 			</div>
 		</div>
 	)
