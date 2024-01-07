2421da1e0707b5ad823979e4562676847ea8b373
jkoren <j.korenstein@gmail.com>
2024-01-07 12:11:57 -0500

    updated heat load analysis

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/Graphs.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/Graphs.tsx
@@ -1,12 +1,26 @@
+import { Home } from '#models/Home.tsx'
 import { AnalysisHeader } from './AnalysisHeader.tsx'
 import { HeatLoad } from './Graphs/HeatLoad.tsx'
-import { StandardDeviationOfUA } from './Graphs/StandardDeviationOfUA.tsx'
 import { WholeHomeUAComparison } from './Graphs/WholeHomeUAComparison.tsx'
 
 export function Graphs() {
+	let home = new Home(
+		'Pietro',
+		'Schirano',
+		'3,000',
+		'63',
+		'65',
+		'Natural Gas',
+		'75',
+		'70',
+		'65',
+		'7',
+	)
 	return (
 		<div className="section-title">
-			Fuel Type: Natural Gas
+			Fuel Type
+			<br />
+			<div className="item-big">{home.fuelType}</div> <br />
 			<AnalysisHeader />
 			<HeatLoad />
 			<WholeHomeUAComparison />
