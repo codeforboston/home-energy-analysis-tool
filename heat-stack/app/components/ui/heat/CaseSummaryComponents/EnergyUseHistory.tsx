a1efbe7012757c52fd311fe123a4ff3db7a5c66b
jkoren <j.korenstein@gmail.com>
2024-01-07 12:11:57 -0500

    updated heat load analysis

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/EnergyUseHistory.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/EnergyUseHistory.tsx
@@ -3,19 +3,6 @@ import { AnalysisHeader } from './AnalysisHeader.tsx'
 import { EnergyUseHistoryChart } from './EnergyUseHistoryChart.tsx'
 
 export function EnergyUseHistory() {
-	let heatingLoadAnalysis = new HeatingLoadAnalysis(
-		'rules_engine_version?',
-		'balance_point_initial?',
-		'60.5',
-		'1.07',
-		'balance_point_sensitivity?',
-		'63.5',
-		'1,112',
-		'5.52',
-		'average_heat_load?',
-		'maximum_heat_load?',
-	)
-
 	return (
 		<div className="section-title">
 			Energy Use History
