d1ab5bdef8c761946ed1632c40bad89a76997a37
Michael <mrmchughes27@gmail.com>
2024-01-26 14:11:29 -0500

    Fixed TypeScript errors in Stories

--- heat-stack/stories/Page.stories.ts
+++ heat-stack/stories/Page.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { within, userEvent, expect } from '@storybook/test';
 
-import { Page } from './Page';
+import { Page } from './Page.tsx';
 
 const meta = {
   title: 'Example/Page',
