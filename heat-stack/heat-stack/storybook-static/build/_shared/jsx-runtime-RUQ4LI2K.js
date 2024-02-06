00f6a4025694f5255e070b7422d4df7e485673f2
Michael <mrmchughes27@gmail.com>
2024-02-06 15:03:49 -0500

    Changed build-storybook command

--- heat-stack/heat-stack/storybook-static/build/_shared/jsx-runtime-RUQ4LI2K.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_jsx_runtime
-} from "/build/_shared/chunk-NMZL6IDN.js";
-import "/build/_shared/chunk-BOXFZXVX.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export default require_jsx_runtime();
-//# sourceMappingURL=/build/_shared/jsx-runtime-RUQ4LI2K.js.map
