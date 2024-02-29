7a6e03a3f2fbe135488172d0ada3bf807b6f539c
Michael <mrmchughes27@gmail.com>
2024-02-29 15:51:00 -0500

    began adding styles to button stories

--- heat-stack/stories/Button.tsx
+++ heat-stack/stories/Button.tsx
@@ -29,17 +29,17 @@ interface ButtonProps {
  * Primary UI component for user interaction
  */
 export const Button = ({
-  primary = false,
   size = 'default',
+  variant,
   backgroundColor,
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${variant}`].join(' ')}
       style={{ backgroundColor }}
       {...props}
     >
