5787b7d891cb2421e49e37c3fd531130cb9f0255
Thad Kerosky <contact@thadk.net>
2024-03-26 22:13:56 -0400

    CurrentHeatingSystem form checking (#158)
    * WIP: explore abstract in zod types
    
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: Clayton Schneider <clayton-schneider@users.noreply.github.com>
    Co-authored-by: Leopardfoot <Leopardfoot@users.noreply.github.com>
    Co-authored-by: Camden Blatchly <camdenblatchly@users.noreply.github.com>
    
    * Added zod types for CurrentHeatingSystem and change types/index.ts to zod.
    
    Co-authored-by: Camden Blatchly <camdenblatchly@users.noreply.github.com>
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    
    * fix display analysis+charts to use zod
    
    ---------
    
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: Clayton Schneider <clayton-schneider@users.noreply.github.com>
    Co-authored-by: Leopardfoot <Leopardfoot@users.noreply.github.com>
    Co-authored-by: Camden Blatchly <camdenblatchly@users.noreply.github.com>

--- heat-stack/app/routes/_heat+/single.tsx
+++ heat-stack/app/routes/_heat+/single.tsx
@@ -9,6 +9,7 @@ import { z } from 'zod'
 
 // Ours
 import { ErrorList } from '#app/components/ui/heat/CaseSummaryComponents/ErrorList.tsx'
+import { Home, Location, Case } from '../../../types/index.ts'
 import { CurrentHeatingSystem } from '../../components/ui/heat/CaseSummaryComponents/CurrentHeatingSystem.tsx'
 import { EnergyUseHistory } from '../../components/ui/heat/CaseSummaryComponents/EnergyUseHistory.tsx'
 import { HomeInformation } from '../../components/ui/heat/CaseSummaryComponents/HomeInformation.tsx'
@@ -20,20 +21,32 @@ const addressMaxLength = 100
 
 /** Modeled off the conform example at
  *     https://github.com/epicweb-dev/web-forms/blob/b69e441f5577b91e7df116eba415d4714daacb9d/exercises/03.schema-validation/03.solution.conform-form/app/routes/users%2B/%24username_%2B/notes.%24noteId_.edit.tsx#L48 */
-const HomeInformationSchema = z.object({
-	name: z.string().min(1).max(nameMaxLength),
-	address: z.string().min(1).max(addressMaxLength),
-	livingSpace: z.number().min(1),
+
+// const HomeInformationSchema = {
+// 	name: z.string().min(1).max(nameMaxLength),
+// 	address: z.string().min(1).max(addressMaxLength),
+// 	livingSpace: z.number().min(1),
+// }
+// // type Home = z.infer<typeof HomeSchema>
+
+// // TODO Next: Ask an LLM how we get fuelType out of HomeSchema from zod
+
+const HomeFormSchema = Home.pick({ livingArea: true })
+	.and(Location.pick({ address: true }))
+	.and(Case.pick({ name: true }))
+
+const CurrentHeatingSystemSchema = Home.pick({
+	fuelType: true,
+	heatingSystemEfficiency: true,
+	thermostatSetPoint: true,
+	setbackTemperature: true,
+	setbackHoursPerDay: true,
+	designTemperatureOverride: true,
 })
 
-const EnergyUseSchema = z.object({
-	fuelType: z.string().min(1).max(nameMaxLength),
-	efficiency: z.string().min(1).max(addressMaxLength),
-	override: z.number().min(1),
-	setpoint: z.string().min(1).max(nameMaxLength),
-	setbackTemp: z.string().min(1).max(addressMaxLength),
-	setbackHours: z.number().min(1),
-})
+const Schema = HomeFormSchema.and(CurrentHeatingSystemSchema)
+
+// const EnergyUseSchema = '';
 
 export async function action({ request, params }: ActionFunctionArgs) {
 	// Checks if url has a homeId parameter, throws 400 if not there
@@ -41,32 +54,38 @@ export async function action({ request, params }: ActionFunctionArgs) {
 
 	const formData = await request.formData()
 	const submission = parseWithZod(formData, {
-		schema: HomeInformationSchema,
+		schema: Schema,
 	})
 
-	if(submission.status !== "success") {
+	if (submission.status !== 'success') {
 		return submission.reply()
-			// submission.reply({
-			// 	// You can also pass additional error to the `reply` method
-			// 	formErrors: ['Submission failed'],
-			// 	fieldErrors: {
-			// 		address: ['Address is invalid'],
-			// 	},
-	
-			// 	// or avoid sending the the field value back to client by specifying the field names
-			// 	hideFields: ['password'],
-			// }),
-			// {status: submission.status === "error" ? 400 : 200}
+		// submission.reply({
+		// 	// You can also pass additional error to the `reply` method
+		// 	formErrors: ['Submission failed'],
+		// 	fieldErrors: {
+		// 		address: ['Address is invalid'],
+		// 	},
+
+		// 	// or avoid sending the the field value back to client by specifying the field names
+		// 	hideFields: ['password'],
+		// }),
+		// {status: submission.status === "error" ? 400 : 200}
 	}
 
 	// TODO NEXT WEEK
 	// - [x] Server side error checking/handling
 	// - [x] ~Save to cookie and redirect to next form~ Put everything on the same page
+	// - [ ] - Get zod and Typescript to play nice
 	// - [ ] (We're here) Build form #2
 	// - [ ] Build form #3
 	// - [ ] Form errors (if we think of a use case - 2 fields conflicting...)
 
-	const { name, address, livingSpace } = submission.value
+	const { name, address, livingArea, fuelType,
+		heatingSystemEfficiency,
+		thermostatSetPoint,
+		setbackTemperature,
+		setbackHoursPerDay,
+		designTemperatureOverride } = submission.value
 
 	// await updateNote({ id: params.noteId, title, content })
 
@@ -78,17 +97,20 @@ export default function Inputs() {
 	const [form, fields] = useForm({
 		lastResult,
 		onValidate({ formData }) {
-			return parseWithZod(formData, { schema: HomeInformationSchema })
+			return parseWithZod(formData, { schema: Schema })
 		},
-		defaultValue: {
-			
-		},
+		defaultValue: {},
 		shouldValidate: 'onBlur',
 	})
 
 	return (
 		<>
-			<Form id={form.id} method="post" onSubmit={form.onSubmit} action="/single">
+			<Form
+				id={form.id}
+				method="post"
+				onSubmit={form.onSubmit}
+				action="/single"
+			>
 				<HomeInformation fields={fields} />
 				<CurrentHeatingSystem fields={fields} />
 				<EnergyUseHistory />
