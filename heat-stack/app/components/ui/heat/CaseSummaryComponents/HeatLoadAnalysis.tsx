9e10bc7bec314f7ebb0ef9056eb3606af2884185
Thad Kerosky <contact@thadk.net>
2024-06-04 20:31:59 -0400

    fix geocoder which was accumulating state unnecessarily (#190)
    * fix geocoder which was accumulating state unnecessarily
    
    Co-authored-by: Camden Blatchly <camdenblatchly@users.noreply.github.com>
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: Leopardfoot <Leopardfoot@users.noreply.github.com>
    
    * switch key variables in types from camelCase to snake_case
    
    * form submits, fix weatherutil append, rm old root.tsx weather, tidying
    
    * fix typechecking
    
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: Jake McCambley <McCambley@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Camden Blatchly <camdenblatchly@users.noreply.github.com>
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: Leopardfoot <Leopardfoot@users.noreply.github.com>
    Co-authored-by: Jake McCambley <McCambley@users.noreply.github.com>

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/HeatLoadAnalysis.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/HeatLoadAnalysis.tsx
@@ -3,7 +3,7 @@ import { HeatLoad } from './Graphs/HeatLoad.tsx'
 import { WholeHomeUAComparison } from './Graphs/WholeHomeUAComparison.tsx'
 
 export function Graphs() {
-	const fuelType = 'Natural Gas'
+	const fuel_type = 'Natural Gas'
 	const titleClassTailwind = 'text-5xl font-extrabold tracking-wide'
 	const componentMargin = 'mt-10'
 	return (
@@ -12,7 +12,7 @@ export function Graphs() {
 				Heat Load Analysis
 			</h2>
 			Fuel Type
-			{fuelType}
+			{fuel_type}
 			<AnalysisHeader />
 			<HeatLoad />
 			<WholeHomeUAComparison />
