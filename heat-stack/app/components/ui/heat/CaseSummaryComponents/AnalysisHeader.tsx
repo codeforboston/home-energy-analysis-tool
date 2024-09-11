f89e3c7dc1acf975ad03e73028aa0b431774ade8
Thad Kerosky <contact@thadk.net>
2024-09-10 22:51:16 -0400

    Dynamic <EnergyUsageHistoryChart/> from Python Rules Engine (#249)
    * Begin sending prop data to usage charts table component
    
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: Leopardfoot <Leopardfoot@users.noreply.github.com>
    
    * Hide some components that don't have data yet, pass indoor temperature from rules engine.
    
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: AbdiladifG <AbdiladifG@users.noreply.github.com>
    
    * hydrate AnalysisHeader with rules-engine data
    
    * hydrate EnergyUseHistoryChart with rules-engine data
    
    * calculate num days in period, conditionally set row background, analysisType image and checkbox state
    
    * set up frame for Override Default checkbox behavior
    
    * linting, tsc, css->tailwind
    
    Co-authored-by: AbdiladifG <AbdiladifG@users.noreply.github.com>
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: dwindleduck <dwindleduck@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: Leopardfoot <Leopardfoot@users.noreply.github.com>
    Co-authored-by: AbdiladifG <AbdiladifG@users.noreply.github.com>
    Co-authored-by: dwindleduck <119227220+dwindleduck@users.noreply.github.com>
    Co-authored-by: dwindleduck <dwindleduck@users.noreply.github.com>

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/AnalysisHeader.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/AnalysisHeader.tsx
@@ -1,35 +1,76 @@
 import { type z } from 'zod'
-import { type HeatLoadAnalysisZod} from '#types/index'
+import { type HeatLoadAnalysisZod } from '#types/index'
 
 type HeatLoadAnalysisZod = z.infer<typeof HeatLoadAnalysisZod>
-export function AnalysisHeader() {
-	const heatLoadAnalysis: HeatLoadAnalysisZod = {
-		rulesEngineVersion: 'Beta 1',
-		estimatedBalancePoint: 60.5,
-		otherFuelUsage: 1.07,
-		averageIndoorTemperature: 68,
-		differenceBetweenTiAndTbp: 0,
-		design_temperature: 0,
-		wholeHomeHeatLossRate: 1112,
-		standardDeviationHeatLossRate: 5.52,
-		averageHeatLoad: 0,
-		maximumHeatLoad: 0,
-	}
+export function AnalysisHeader(props: { usage_data: any }) {
+
+	// Example usage_data
+	// new Map([[
+	// 		"estimated_balance_point",
+	// 		61.5
+	// 	],[
+	// 		"other_fuel_usage",
+	// 		0.2857142857142857
+	// 	],[
+	// 		"average_indoor_temperature",
+	// 		67
+	// 	],[
+	// 		"difference_between_ti_and_tbp",
+	// 		5.5
+	// 	],[
+	// 		"design_temperature",
+	// 		1
+	// 	],[
+	// 		"whole_home_heat_loss_rate",
+	// 		48001.81184312083
+	// 	],[
+	// 		"standard_deviation_of_heat_loss_rate",
+	// 		0.08066745182677547
+	// 	],[
+	// 		"average_heat_load",
+	// 		3048115.0520381727
+	// 	],[
+	// 		"maximum_heat_load",
+	// 		3312125.0171753373
+	// 	]])
+
+	const summaryOutputs = props.usage_data?.get('summary_output')
+
+	// Calculate the number of billing periods included in Heating calculations
+	const heatingAnalysisTypeRecords = props.usage_data
+		?.get('billing_records')
+		?.filter((billingRecord: any) => billingRecord.get('analysis_type') == 1)
+
+	const recordsIncludedByDefault = heatingAnalysisTypeRecords?.filter(
+		(billingRecord: any) =>
+			billingRecord.get('default_inclusion_by_calculation') == true &&
+			billingRecord.get('inclusion_override') == false,
+	).length
+
+	const recordsIncludedByOverride = heatingAnalysisTypeRecords?.filter(
+		(billingRecord: any) =>
+			billingRecord.get('default_inclusion_by_calculation') == false &&
+			billingRecord.get('inclusion_override') == true,
+	).length
+
+	const numRecordsForHeatingCalculations =
+		recordsIncludedByDefault + recordsIncludedByOverride
 
 	return (
 		<div className="section-title">
-			<div className="item-group-title">Analysis</div>
+			<div className="item-group-title">Heat Load Analysis</div>
 			<div className="flex flex-row">
 				<div className="basis-1/3">
 					<div className="item-title-small">
 						Average Indoor Temperature <br />
 						<div className="item">
-							{heatLoadAnalysis.averageIndoorTemperature} °F
+							{summaryOutputs?.get('average_indoor_temperature')} °F
 						</div>{' '}
 						<br />
-						Balance Point Temperature (°F) <br />
+						Balance Point Temperature
+						<br />
 						<div className="item">
-							{heatLoadAnalysis.estimatedBalancePoint}
+							{summaryOutputs?.get('estimated_balance_point')} °F
 						</div>{' '}
 						<br />
 					</div>
@@ -37,11 +78,12 @@ export function AnalysisHeader() {
 				<div className="basis-1/3">
 					<div className="item-title-small">
 						Number of Periods Included <br />
-						<div className="item">(to be calculated)</div>
+						<div className="item">{numRecordsForHeatingCalculations}</div>
 						<br />
 						Daily non-heating Usage <br />
 						<div className="item">
-							{heatLoadAnalysis.otherFuelUsage} therms
+							{/* Rounding to two decimal places */}
+							{summaryOutputs?.get('other_fuel_usage').toFixed(2)} therms
 						</div>{' '}
 					</div>
 				</div>
@@ -49,13 +91,20 @@ export function AnalysisHeader() {
 					<div className="item-title-small">
 						Standard Deviation of UA <br />
 						<div className="item">
-							{heatLoadAnalysis.standardDeviationHeatLossRate} %
+							{/* Rounding to two decimal places */}
+							{(
+								summaryOutputs?.get('standard_deviation_of_heat_loss_rate') *
+								100
+							).toFixed(2)}{' '}
+							%
 						</div>{' '}
 						<br />
 						Whole-home UA
 						<br />
 						<div className="item">
-							{heatLoadAnalysis.wholeHomeHeatLossRate} BTU/h-°F
+							{/* Rounding to zero decimal places */}
+							{summaryOutputs?.get('whole_home_heat_loss_rate').toFixed(0)}{' '}
+							BTU/h-°F
 						</div>{' '}
 						<br />
 					</div>
