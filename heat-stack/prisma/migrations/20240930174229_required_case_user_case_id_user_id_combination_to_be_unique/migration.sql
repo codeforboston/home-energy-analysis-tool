12d6ee474fc2c9231083568798e95c7d52fd3efa
Ethan Strominger <ethanstrominger2@gmail.com>
2024-10-04 13:00:06 -0400

    Remigrate scripts to have two clean scripts

--- heat-stack/prisma/migrations/20240930174229_required_case_user_case_id_user_id_combination_to_be_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[caseId,userId]` on the table `CaseUser` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "CaseUser_caseId_userId_key" ON "CaseUser"("caseId", "userId");
