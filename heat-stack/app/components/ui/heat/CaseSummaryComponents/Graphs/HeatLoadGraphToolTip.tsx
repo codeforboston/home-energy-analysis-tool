9f9fa3671d166604198336347bd8cc1921f33e0a
derekvmcintire <derekcommonground@gmail.com>
2025-01-09 13:16:37 -0500

    fix HeatLoadGraphToolTip type error

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/Graphs/HeatLoadGraphToolTip.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/Graphs/HeatLoadGraphToolTip.tsx
@@ -1,8 +1,5 @@
-import React from 'react'
-import { TooltipProps, TooltipItem } from 'recharts'
-
 type HeatLoadGraphToolTipProps = {
-	payload: TooltipItem[]
+    payload?: Array<{ payload: { temperature?: number }; value?: number; name?: string }>
 }
 
 /**
@@ -11,30 +8,28 @@ type HeatLoadGraphToolTipProps = {
  * @returns {JSX.Element} - The rendered tooltip element.
  */
 export const HeatLoadGraphToolTip = (
-	props: HeatLoadGraphToolTipProps,
+    props: HeatLoadGraphToolTipProps,
 ): JSX.Element => {
-	const { payload } = props
-	const temperature = payload ? payload[0]?.payload?.temperature : null
-	const value = payload ? payload[0]?.value : null
-	const name = payload && payload[0] ? payload[0].name : ''
+    const { payload } = props
+    const temperature = payload?.[0]?.payload?.temperature ?? null
+    const value = payload?.[0]?.value ?? null
+    const name = payload?.[0]?.name ?? ''
 
-	if (temperature !== null) {
-		return (
-			<div className="tooltip-content rounded border border-gray-300 bg-white p-2">
-				<div>{`${Number(value).toLocaleString()} BTU/h`}</div>
-				<div>{`${temperature}°F ${name?.replace('Line', ' Heat Load').replace('Point', ' at Design Temperature')}`}</div>
-			</div>
-		)
-	}
+    if (temperature !== null) {
+        return (
+            <div className="tooltip-content rounded border border-gray-300 bg-white p-2">
+                <div>{`${Number(value).toLocaleString()} BTU/h`}</div>
+                <div>{`${temperature}°F ${name.replace('Line', ' Heat Load').replace('Point', ' at Design Temperature')}`}</div>
+            </div>
+        )
+    }
 
-	return (
-		<div className="tooltip-content rounded border border-gray-300 bg-white p-2">
-			<div>{`${Number(value).toLocaleString()} BTU/h`}</div>
-			<div>
-				{name
-					?.replace('Line', ' Heat Load')
-					.replace('Point', ' at Design Temperature')}
-			</div>
-		</div>
-	)
+    return (
+        <div className="tooltip-content rounded border border-gray-300 bg-white p-2">
+            <div>{`${Number(value).toLocaleString()} BTU/h`}</div>
+            <div>
+                {name.replace('Line', ' Heat Load').replace('Point', ' at Design Temperature')}
+            </div>
+        </div>
+    )
 }
