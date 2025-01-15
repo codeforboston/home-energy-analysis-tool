ab290246b6b6b51409a2186e182e74a344080df6
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2025-01-14 20:42:54 -0500

    223 heatload graph (#288)
    * Use rules engine output to populate heat load graph
    
    * update graph record type
    
    * run prettier for select files
    
    * fix dataKeys
    
    * Use correct data and make chart pretty
    
    * Graph working, but needs clean up
    
    * Clean up and comments
    
    * Comments on calculation functions
    
    * Add icon, still need to add values underneath chart
    
    * restore ws changes to types
    
    * Add columns at bottom of chart
    
    * Fix reversed average and max in grid
    
    * Fix responsiveness issues
    
    * Extract utility and data building functions, tool tip and constants
    
    * Add comment with link to external calculation docs
    
    * Fix tool tip
    
    * quick refactor
    
    * clean up
    
    * refactor calculating min and max for Y axis
    
    * set start and end temps once
    
    * Add 2f to x axis so tool tip is easier to invoke
    
    * review changes to HeatLoad.tsx
    
    * All issues except legend and tool type type errors addressed
    
    * legend working with valid value for layout
    
    * break out legend and fix avg heat load endpoint
    
    * Adjust x axis ticks and make design set point a global constant
    
    * fix HeatLoadGraphToolTip type error
    
    * Remove hard coded data
    
    * Add comment to calculateAvgHeatLoadEndPoint
    
    * comment out inclusion_override
    
    * Fix type errors
    
    * re-comment tsconfig

---
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/constants.ts
@@ -0,0 +1,6 @@
+// Constants for chart styling
+export const COLOR_ORANGE = '#FF5733'
+export const COLOR_BLUE = '#8884d8'
+export const COLOR_GREY = '#999999'
+export const COLOR_GREY_LIGHT = '#cccccc'
+export const COLOR_WHITE = '#fff'
