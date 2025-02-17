3e82a8b55d329d0fb29b651ca39efee5b0cee834
Thad Kerosky <contact@thadk.net>
2025-02-17 18:30:35 -0500

    Checkboxes re-run Rules Engine (#295)
    * created Object to hold usageData, refactored Inputs function to eliminate redundancies
    
    Co-authored-by: Derek McIntire <derekvmcintire@users.noreply.github.com>
    
    * extracted rules-engine code to app/utils/rules-engine.ts
    
    Co-authored-by: Derek McIntire <derekvmcintire@users.noreply.github.com>
    
    * extracted data parsing functions to /app/utils/data-parser.ts
    
    Co-authored-by: Derek McIntire <derekvmcintire@users.noreply.github.com>
    
    * comment cleanup
    
    Co-authored-by: Derek McIntire <derekvmcintire@users.noreply.github.com>
    
    * extracted file upload functionality
    
    Co-authored-by: Derek McIntire <derekvmcintire@users.noreply.github.com>
    
    * need to figure out how to trigger recalc
    
    * extracted date and weather parsing logic to date-temp-util
    
    * Pseudo code for updating billing records
    
    * extract date and weather parsing to date-temp-util
    
    * fix conditional display logic for EnergyUseHistory component
    
    * add actionCounter, return pyodide Parser call to action, add comments
    
    * create EnergyUseUpload component, rename Submit button as Re-calculate
    
    * shell for new route for /recalculate
    
    * on checkbox, run autonomous executeRoundtripAnalyticsFromFormJs() separate from form single.tsx's action.
    
    Co-authored-by: dwindleduck <dwindleduck@users.noreply.github.com>
    Co-authored-by: Ethan-Strominger <ethanstrominger@users.noreply.github.com>
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: Derek McIntire <derekvmcintire@users.noreply.github.com>
    
    * linting
    
    * cleanup after team review, tabs->spaces
    
    ---------
    
    Co-authored-by: dwindleduck <119227220+dwindleduck@users.noreply.github.com>
    Co-authored-by: Derek McIntire <derekvmcintire@users.noreply.github.com>
    Co-authored-by: derekvmcintire <derekcommonground@gmail.com>
    Co-authored-by: dwindleduck <dwindleduck@users.noreply.github.com>
    Co-authored-by: Ethan-Strominger <ethanstrominger@users.noreply.github.com>
    Co-authored-by: plocket <plocket@users.noreply.github.com>

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/EnergyUseHistory.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/EnergyUseHistory.tsx
@@ -1,9 +1,10 @@
 import { Upload } from 'lucide-react'
 
 import { Button } from '#/app/components/ui/button.tsx'
-import { type UsageDataSchema } from '#/types/types.ts'; 
+import { BillingRecordsSchema, type UsageDataSchema } from '#/types/types.ts'; 
 import { AnalysisHeader } from './AnalysisHeader.tsx'
 import { EnergyUseHistoryChart } from './EnergyUseHistoryChart.tsx'
+import { EnergyUseUpload } from './EnergyUseUpload.tsx';
 
 // import { FieldMetadata, useForm } from '@conform-to/react'
 // import { Form, useLocation } from '@remix-run/react'
@@ -11,11 +12,29 @@ import { EnergyUseHistoryChart } from './EnergyUseHistoryChart.tsx'
 // import { Input } from '#/app/components/ui/input.tsx'
 // import { Label } from '#/app/components/ui/label.tsx'
 
+interface EnergyUseHistoryProps 
+{
+	lastResult: any;
+	parsedLastResult: Map<any, any> | undefined;
+	usage_data: UsageDataSchema;
+	recalculateFn: (
+		parsedLastResult: Map<any, any> | undefined,
+		billingRecords: BillingRecordsSchema,
+		parsedAndValidatedFormSchema: any,
+        convertedDatesTIWD: any,
+        state_id: any,
+        county_id: any
+	) => void;
+	show_usage_data: boolean
+}
+
 export function EnergyUseHistory({
+	lastResult,
+	parsedLastResult,
 	usage_data,
-}: {
-	usage_data: UsageDataSchema
-}) {
+	recalculateFn,
+	show_usage_data
+}: EnergyUseHistoryProps) {
 	const titleClass = 'text-5xl font-extrabold tracking-wide mt-10'
 	// const subtitleClass = 'text-2xl font-semibold text-zinc-950 mt-9'
 
@@ -23,28 +42,13 @@ export function EnergyUseHistory({
 		<div>
 			<h2 className={`${titleClass} pb-6`}>Energy Use History</h2>
 
-			<input
-				id="energy_use_upload"
-				aria-label="Upload your energy billing company's bill."
-				accept=".xml,.csv,application/xml,text/xml,text/csv,application/csv,application/x-csv,text/comma-separated-values,text/x-comma-separated-values"
-				name="energy_use_upload"
-				type="file"
-			/>
-			<Button type="submit">
-				<Upload className="mr-2 h-4 w-4" /> Upload
-			</Button>
+			
 
-			<a
-				className="inline-flex items-center gap-1 rounded-md border border-transparent bg-secondary px-2.5 py-0.5 text-xs font-semibold text-secondary-foreground transition-colors hover:bg-secondary/80 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
-				href="https://github.com/codeforboston/home-energy-analysis-tool/issues/162#issuecomment-2246594484"
-			>
-				Get example file here
-			</a>
-
-			{usage_data && (
+			{show_usage_data && (
 				<>
 					<AnalysisHeader usage_data={usage_data} />
-					<EnergyUseHistoryChart usage_data={usage_data} />
+					<EnergyUseHistoryChart usage_data={usage_data} lastResult={lastResult} parsedLastResult={parsedLastResult} recalculateFn={recalculateFn}/>
+					<Button type="submit">Re-calculate</Button>
 				</>
 			)}
 		</div>
