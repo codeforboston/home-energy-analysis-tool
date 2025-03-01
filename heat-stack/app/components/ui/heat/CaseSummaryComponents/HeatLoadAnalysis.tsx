b7f273a95f650f27b0baf98089068d550f99e56e
Thad Kerosky <contact@thadk.net>
2025-03-01 14:38:16 -0500

    missed react_router_v7 fix commits removing HeatLoadAnalysis route an… (#306)
    * missed react_router_v7 fix commits removing HeatLoadAnalysis route and replacing with renamed Graph component
    
    * delete heat-load-analysis route

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/HeatLoadAnalysis.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/HeatLoadAnalysis.tsx
@@ -1,13 +1,14 @@
+// heat-stack/app/components/ui/heat/CaseSummaryComponents/HeatLoadAnalysis.tsx
 // import { AnalysisHeader } from './AnalysisHeader.tsx'
 import { SummaryOutputSchema } from '#types/types.ts';
 import { HeatLoad } from './Graphs/HeatLoad.tsx'
 import { WholeHomeUAComparison } from './Graphs/WholeHomeUAComparison.tsx'
 
 interface GraphsProps {
-	heatLoadSummaryOutput: SummaryOutputSchema | undefined;
+	heatLoadSummaryOutput: SummaryOutputSchema;
 }
 
-export function Graphs({ heatLoadSummaryOutput }: GraphsProps) {
+export function HeatLoadAnalysis({ heatLoadSummaryOutput }: GraphsProps) {
 	const fuel_type = 'Natural Gas'
 	const titleClassTailwind = 'text-5xl font-extrabold tracking-wide'
 	const componentMargin = 'mt-10'
