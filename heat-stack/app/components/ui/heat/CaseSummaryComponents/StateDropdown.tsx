dc40a4e58fde795a4a1f1a3568f4652e2af71406
AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>
2025-05-30 14:50:28 -0400

    Enabled typing to select state
    Co-authored-by: Ethan-Strominger <32078396+ethanstrominger@users.noreply.github.com>

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/StateDropdown.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/StateDropdown.tsx
@@ -1,16 +1,71 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Select, { SingleValue, ActionMeta } from 'react-select'
 
-const people = [
-	{ value: 1, label: 'Durward Reynolds' },
-	{ value: 2, label: 'Kenton Towne' },
-	{ value: 3, label: 'Therese Wunsch' },
-	{ value: 4, label: 'Benedict Kessler' },
-	{ value: 5, label: 'Katelyn Rohan' },
-]
+const states = [
+  { value: 'AL', label: 'AL' },
+  { value: 'AK', label: 'AK' },
+  { value: 'AZ', label: 'AZ' },
+  { value: 'AR', label: 'AR' },
+  { value: 'CA', label: 'CA' },
+  { value: 'CO', label: 'CO' },
+  { value: 'CT', label: 'CT' },
+  { value: 'DE', label: 'DE' },
+  { value: 'FL', label: 'FL' },
+  { value: 'GA', label: 'GA' },
+  { value: 'HI', label: 'HI' },
+  { value: 'ID', label: 'ID' },
+  { value: 'IL', label: 'IL' },
+  { value: 'IN', label: 'IN' },
+  { value: 'IA', label: 'IA' },
+  { value: 'KS', label: 'KS' },
+  { value: 'KY', label: 'KY' },
+  { value: 'LA', label: 'LA' },
+  { value: 'ME', label: 'ME' },
+  { value: 'MD', label: 'MD' },
+  { value: 'MA', label: 'MA' },
+  { value: 'MI', label: 'MI' },
+  { value: 'MN', label: 'MN' },
+  { value: 'MS', label: 'MS' },
+  { value: 'MO', label: 'MO' },
+  { value: 'MT', label: 'MT' },
+  { value: 'NE', label: 'NE' },
+  { value: 'NV', label: 'NV' },
+  { value: 'NH', label: 'NH' },
+  { value: 'NJ', label: 'NJ' },
+  { value: 'NM', label: 'NM' },
+  { value: 'NY', label: 'NY' },
+  { value: 'NC', label: 'NC' },
+  { value: 'ND', label: 'ND' },
+  { value: 'OH', label: 'OH' },
+  { value: 'OK', label: 'OK' },
+  { value: 'OR', label: 'OR' },
+  { value: 'PA', label: 'PA' },
+  { value: 'RI', label: 'RI' },
+  { value: 'SC', label: 'SC' },
+  { value: 'SD', label: 'SD' },
+  { value: 'TN', label: 'TN' },
+  { value: 'TX', label: 'TX' },
+  { value: 'UT', label: 'UT' },
+  { value: 'VT', label: 'VT' },
+  { value: 'VA', label: 'VA' },
+  { value: 'WA', label: 'WA' },
+  { value: 'WV', label: 'WV' },
+  { value: 'WI', label: 'WI' },
+  { value: 'WY', label: 'WY' },
+];
 
-export function StateDropdown() {
-	const [selectedPerson, setSelectedPerson] =
+interface StateDropdownProps {
+	fields: any
+	getInputProps: any
+	subSubTitleClass: string
+}
+
+export function StateDropdown({
+	fields,
+	getInputProps,
+	subSubTitleClass,
+}: StateDropdownProps) {
+	const [selectedState, setSelectedState] =
 		useState<SingleValue<{ value: number; label: string }>>(null)
 	const [query, setQuery] = useState('')
 
@@ -22,24 +77,26 @@ export function StateDropdown() {
 		selectedOption: SingleValue<{ value: number; label: string }>,
 		actionMeta: ActionMeta<{ value: number; label: string }>,
 	) => {
-		setSelectedPerson(selectedOption)
+		setSelectedState(selectedOption)
 	}
 
-	const filteredPeople =
+	const filteredStates =
 		query === ''
-			? people
-			: people.filter((person) =>
-					person.label.toLowerCase().includes(query.toLowerCase()),
+			? states
+			: states.filter((states) =>
+					states.label.toLowerCase().includes(query.toLowerCase()),
 				)
 
 	return (
 		<Select
-			value={selectedPerson}
+			value={selectedState}
 			onChange={handleChange}
 			onInputChange={handleInputChange}
-			options={filteredPeople}
+			options={filteredStates}
 			isSearchable
-			placeholder="Select a person"
+			placeholder="Type to select a state"
+			className={subSubTitleClass}
+			{...getInputProps(fields.state, { type: 'text' })}
 		/>
 	)
 }
