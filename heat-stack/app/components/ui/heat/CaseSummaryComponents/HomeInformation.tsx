111dfa3ca1af51142134aad48018646f302e46c3
AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>
2025-05-30 12:01:38 -0400

    Changed font sizes
    Co-authored-by: Ethan-Strominger <32078396+ethanstrominger@users.noreply.github.com>

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/HomeInformation.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/HomeInformation.tsx
@@ -49,6 +49,7 @@ type HomeInformationProps = { fields: any }
 export function HomeInformation(props: HomeInformationProps) {
 	const titleClass = 'text-4xl font-bold tracking-wide'
 	const subtitleClass = 'text-2xl font-semibold text-zinc-950 mt-9'
+	const subSubTitleClass = 'text-sm font-semibold text-zinc-950 mt-4'
 	const descriptiveClass = 'mt-2 text-sm text-slate-500'
 	const componentMargin = 'mt-10'
 
@@ -90,27 +91,28 @@ export function HomeInformation(props: HomeInformationProps) {
 			<Label className={`${subtitleClass}`} htmlFor="name">
 				Resident/Client Name(s)
 			</Label>
-				{/* <Form method="post" action="/inputs1"> */}
-				<div className={`${componentMargin}`}>
-					<div className="mt-4 flex space-x-4">
-						<div>
-							<Input {...getInputProps(props.fields.name, { type: 'text' })} />
-							<div className="min-h-[32px] px-4 pb-3 pt-1">
-								<ErrorList
-									id={props.fields.name.errorId}
-									errors={props.fields.name.errors}
-								/>
-							</div>
+			{/* <Form method="post" action="/inputs1"> */}
+			<div className={`${componentMargin}`}>
+				<div className="mt-4 flex space-x-4">
+					<div>
+						<Input {...getInputProps(props.fields.name, { type: 'text' })} />
+						<div className="min-h-[32px] px-4 pb-3 pt-1">
+							<ErrorList
+								id={props.fields.name.errorId}
+								errors={props.fields.name.errors}
+							/>
 						</div>
 					</div>
 				</div>
-			<div className="mt-4 flex space-x-4">
-				<div className="mt-9">
-					<Label className={`${subtitleClass}`} htmlFor="street_address">
-						Street Address
-					</Label>
-					<div className="mt-4 flex space-x-4">
-						<div>
+			</div>
+			<fieldset>
+				<legend className={`${subtitleClass}`}>Address Information</legend>
+				<div className="mt-4 flex space-x-4">
+					<div className="basis-1/3">
+						<Label className={`${subSubTitleClass}"`} htmlFor="street_address">
+							Street Address
+						</Label>
+						<div className="mt-4">
 							<Input {...getInputProps(props.fields.street_address, { type: 'text' })} />
 							<div className="min-h-[32px] px-4 pb-3 pt-1">
 								<ErrorList
@@ -120,14 +122,12 @@ export function HomeInformation(props: HomeInformationProps) {
 							</div>
 						</div>
 					</div>
-				</div>
 
-				<div className="mt-9">
-					<Label className={`${subtitleClass}`} htmlFor="town">
-						Town
-					</Label>
-					<div className="mt-4 flex space-x-4">
-						<div>
+					<div className="basis-1/3">
+						<Label className={`${subSubTitleClass}`} htmlFor="town">
+							Town
+						</Label>
+						<div className="mt-4">
 							<Input {...getInputProps(props.fields.town, { type: 'text' })} />
 							<div className="min-h-[32px] px-4 pb-3 pt-1">
 								<ErrorList
@@ -137,15 +137,13 @@ export function HomeInformation(props: HomeInformationProps) {
 							</div>
 						</div>
 					</div>
-				</div>
 
-				{/* TODO: Add dropdown of state abbreviation suggestions */}
-				<div className="mt-9">
-					<Label className={`${subtitleClass}`} htmlFor="state">
-						State (Abbreviation)
-					</Label>
-					<div className="mt-4 flex space-x-4">
-						<div>
+					{/* TODO: Add dropdown of state abbreviation suggestions */}
+					<div className="basis-1/3">
+						<Label className={`${subSubTitleClass}`} htmlFor="state">
+							State (Abbreviation)
+						</Label>
+						<div className="mt-4">
 							<Input {...getInputProps(props.fields.state, { type: 'text' })} />
 							<div className="min-h-[32px] px-4 pb-3 pt-1">
 								<ErrorList
@@ -156,7 +154,7 @@ export function HomeInformation(props: HomeInformationProps) {
 						</div>
 					</div>
 				</div>
-			</div>
+			</fieldset>
 
 			<div className="mt-9">
 				<Label className={`${subtitleClass}`} htmlFor="living_area">
