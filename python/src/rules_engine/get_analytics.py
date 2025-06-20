54a9e215ec23c60b7ed323a213cc274b50ce6023
Ethan Strominger <ethanstrominger2@gmail.com>
2025-06-20 16:38:37 -0400

    WIP major refactor of python

--- python/src/rules_engine/get_analytics.py
+++ python/src/rules_engine/get_analytics.py
@@ -6,7 +6,7 @@ from rules_engine.pydantic_models import (
 )
 from rules_engine import engine, helpers
 
-def get_analytics(summary_input, temperature_input, csv_data, state_id, county_id):
+def get_analytics(form_data, temperature_input, csv_data):
     """
     second step: this will be the first time to draw the table
     # two new geocode parameters may be needed for design temp:
@@ -20,7 +20,16 @@ def get_analytics(summary_input, temperature_input, csv_data, state_id, county_i
 
     # design_temp_looked_up = helpers.get_design_temp(state_id, county_id)
     # summaryInput = HeatLoadInput(**summaryInputFromJs, design_temperature=design_temp_looked_up)
+    heat_load_input = HeatLoadInput(
+        living_area=form_data["living_area"],
+        fuel_type=form_data["fuel_type"],
+        heating_system_efficiency=form_data["heating_system_efficiency"],
+        thermostat_set_point=form_data["thermostat_set_point"],
+        setback_temperature=form_data.get("setback_temperature"),
+        setback_hours_per_day=form_data.get("setback_hours_per_day"),
+        design_temperature=form_data["design_temperature"],
+    )
 
 
-    outputs = engine.get_outputs_natural_gas(summary_input=summary_input, temperature_input=temperature_input, natural_gas_billing_input=natural_gas_input_records)
+    outputs = engine.get_outputs_natural_gas(heat_load_input=heat_load_input, temperature_input=temperature_input, natural_gas_billing_input=natural_gas_input_records)
     return outputs.model_dump(mode="json")
