0d88b0c290d1de9cd61caf3b212b087e2864e932
Ethan-Strominger <32078396+ethanstrominger@users.noreply.github.com>
2025-06-26 11:38:57 -0400

    Consolidate into one commit from main (#447)
    * Consolidate into. one commit off of main
    
    * Implement pull request
    
    Co-authored-by: AdamFinkle <AdamFinkle@users.noreply.github.com>
    
    * Change error message
    
    * Decapitalized u in UsaStateAbbrev in arrow function
    
    Co-authored-by: Ethan-Strominger <32078396+ethanstrominger@users.noreply.github.com>
    
    * Formatted geocode.ts
    
    Co-authored-by: Ethan-Strominger <32078396+ethanstrominger@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: AdamFinkle <AdamFinkle@users.noreply.github.com>
    Co-authored-by: AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>

---
+++ heat-stack/app/routes/geocode.ts
@@ -0,0 +1,40 @@
+// app/routes/api/geocode.ts
+// import { type LoaderFunctionArgs } from '@remix-run/node'
+import Geocode from '#/app/utils/GeocodeUtil.ts' // Make sure this file is server-only
+import { type Route } from './+types/geocode.ts'
+
+export async function loader({ request }: Route.LoaderArgs) {
+	const url = new URL(request.url)
+	const address = url.searchParams.get('address')
+
+	if (!address) {
+		return new Response(
+			JSON.stringify({ status: 'error', message: 'Missing address parameter' }),
+			{
+				status: 400,
+				headers: { 'Content-Type': 'application/json' },
+			},
+		)
+	}
+
+	try {
+		const geocodeUtil = new Geocode()
+		const { coordinates } = await geocodeUtil.getLL(address)
+
+		if (!coordinates) {
+			return new Response(
+				JSON.stringify({
+					message: 'Address, town. and state combination not found',
+					status: 400,
+				}),
+			)
+		}
+
+		return new Response(JSON.stringify({ coordinates }))
+	} catch (error) {
+		console.error('Geocoding error:', error)
+		return new Response(
+			JSON.stringify({ message: 'Server error', status: 500 }),
+		)
+	}
+}
