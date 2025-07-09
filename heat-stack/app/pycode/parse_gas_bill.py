0221e008818d83053b78a095a2e0f7537cfe0191
AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>
2025-07-09 17:45:49 -0400

    Modified pre-commit-config.yaml
    Co-authored-by: Ethan-Strominger <32078396+ethanstrominger@users.noreply.github.com>

--- heat-stack/app/pycode/parse_gas_bill.py
+++ heat-stack/app/pycode/parse_gas_bill.py
@@ -1,10 +1,7 @@
-from rules_engine import parser
-from rules_engine.pydantic_models import (
-    FuelType,
-    HeatLoadInput,
-    TemperatureInput
-)
-from rules_engine import engine
+from rules_engine import engine, parser
+from rules_engine.pydantic_models import (FuelType, HeatLoadInput,
+                                          TemperatureInput)
+
 
 def executeParse(csvDataJs):
     naturalGasInputRecords = parser.parse_gas_bill(csvDataJs)
--- heat-stack/app/pycode/roundtrip_analytics.py
+++ heat-stack/app/pycode/roundtrip_analytics.py
@@ -1,32 +1,37 @@
-from rules_engine import parser
-from rules_engine.pydantic_models import (
-    FuelType,
-    HeatLoadInput,
-    TemperatureInput,
-    ProcessedEnergyBillInput
-)
-from rules_engine import engine, helpers
+from rules_engine import engine, helpers, parser
+from rules_engine.pydantic_models import (FuelType, HeatLoadInput,
+                                          ProcessedEnergyBillInput,
+                                          TemperatureInput)
 
 
-def executeRoundtripAnalyticsFromForm(summaryInputJs, temperatureInputJs, userAdjustedData, state_id, county_id):
+def executeRoundtripAnalyticsFromForm(
+    summaryInputJs, temperatureInputJs, userAdjustedData, state_id, county_id
+):
     """
     "processed_energy_bills" is the "roundtripping" parameter to be passed as userAdjustedData.
     """
-    
+
     summaryInputFromJs = summaryInputJs.as_object_map().values()._mapping
     temperatureInputFromJs = temperatureInputJs.as_object_map().values()._mapping
 
     design_temp_looked_up = helpers.get_design_temp(state_id, county_id)
     # expect 1 for middlesex county:  print("design temp check ",design_temp_looked_up, state_id, county_id)
-    summaryInput = HeatLoadInput(**summaryInputFromJs, design_temperature=design_temp_looked_up)
+    summaryInput = HeatLoadInput(
+        **summaryInputFromJs, design_temperature=design_temp_looked_up
+    )
 
     temperatureInput = TemperatureInput(**temperatureInputFromJs)
 
     # third step, re-run of the table data
-    userAdjustedDataFromJsToPython = [ProcessedEnergyBillInput(**record) for record in userAdjustedData['processed_energy_bills']]
+    userAdjustedDataFromJsToPython = [
+        ProcessedEnergyBillInput(**record)
+        for record in userAdjustedData["processed_energy_bills"]
+    ]
     # print("py", userAdjustedDataFromJsToPython[0])
 
-    outputs2 = engine.get_outputs_normalized(summaryInput, None, temperatureInput, userAdjustedDataFromJsToPython)
+    outputs2 = engine.get_outputs_normalized(
+        summaryInput, None, temperatureInput, userAdjustedDataFromJsToPython
+    )
 
     # print("py2", outputs2.processed_energy_bills[0])
     return outputs2.model_dump(mode="json")
