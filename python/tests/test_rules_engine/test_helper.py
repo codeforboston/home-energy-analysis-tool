944567e1f580e635cd76615dd7a83368c92628ce
Ben Nikula <ben@yospace.com>
2025-07-12 20:46:04 -0400

    Format using black

--- python/tests/test_rules_engine/test_helper.py
+++ python/tests/test_rules_engine/test_helper.py
@@ -4,11 +4,13 @@ from rules_engine.helpers import get_design_temp
 
 
 @pytest.mark.parametrize("state_id,county_id,expected_result", [["01", "007", 26]])
-def test_get_design_temp_good_value(state_id: str, county_id: str, expected_result: int) -> None:
+def test_get_design_temp_good_value(
+    state_id: str, county_id: str, expected_result: int
+) -> None:
     res = get_design_temp(state_id, county_id)
     assert res == expected_result
 
 
 def test_get_design_temp_bad_value():
     with pytest.raises(ValueError) as e:
-        get_design_temp('abc', None)
+        get_design_temp("abc", None)
