ccd2ab95862b0917100481fdd2673295f302ae3e
plocket <52798256+plocket@users.noreply.github.com>
2025-07-16 13:46:22 -0400

    Redirect from /cases to /login if needed (#477)
    Ammend this commit as we make more progress
    
    Co-authored-by: nikulabs <nikulabs@users.noreply.github.com>
    Co-authored-by: AdamFinkle <AdamFinkle@users.noreply.github.com>
    Co-authored-by: interianow <29824849+interianow@users.noreply.github.com>

--- heat-stack/app/routes/cases+/index.tsx
+++ heat-stack/app/routes/cases+/index.tsx
@@ -2,8 +2,10 @@ import { data, useLoaderData, Link } from 'react-router'
 import { format } from 'date-fns'
 import { prisma } from '#app/utils/db.server.ts'
 import { type Route } from './+types/index.ts'
+import { getUserId } from '#app/utils/auth.server.ts'
 
-export async function loader() {
+export async function loader({ request }: Route.LoaderArgs) {
+    const userID = getUserId(request)
     // Fetch all cases with their related data
     const cases = await prisma.case.findMany({
         include: {
