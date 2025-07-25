8bba8e282e8a8ffe575c6b49c9f205d8b4a94340
nikulabs <nikulabs@gmail.com>
2025-07-25 14:43:50 -0400

    Revert "Added prettier and reformatted .tsx codebase (#515)"
    This reverts commit 72a708923e0277354992775b4c55f977385b81c6.

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/AnalysisHeader.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/AnalysisHeader.tsx
@@ -1,77 +1,77 @@
-import { type z } from "zod";
-import { useEffect, useRef } from "react";
-import { type UsageDataSchema } from "#/types/types.ts";
-import HelpCircle from "./assets/help-circle.svg";
+import { type z } from 'zod';
+import { useEffect, useRef } from 'react';
+import { type UsageDataSchema } from '#/types/types.ts'; 
+import HelpCircle from './assets/help-circle.svg'
 
 interface AnalysisHeaderProps {
-  usageData: UsageDataSchema;
-  scrollAfterSubmit: boolean;
-  setScrollAfterSubmit: React.Dispatch<React.SetStateAction<boolean>>;
+	usageData: UsageDataSchema;
+	scrollAfterSubmit: boolean;
+	setScrollAfterSubmit: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export function AnalysisHeader({
-  usageData,
-  scrollAfterSubmit,
-  setScrollAfterSubmit,
-}: AnalysisHeaderProps) {
-  // Example usageData
-  // new Map([[
-  // 		"estimated_balance_point",
-  // 		61.5
-  // 	],[
-  // 		"other_fuel_usage",
-  // 		0.2857142857142857
-  // 	],[
-  // 		"average_indoor_temperature",
-  // 		67
-  // 	],[
-  // 		"difference_between_ti_and_tbp",
-  // 		5.5
-  // 	],[
-  // 		"design_temperature",
-  // 		1
-  // 	],[
-  // 		"whole_home_heat_loss_rate",
-  // 		48001.81184312083
-  // 	],[
-  // 		"standard_deviation_of_heat_loss_rate",
-  // 		0.08066745182677547
-  // 	],[
-  // 		"average_heat_load",
-  // 		3048115.0520381727
-  // 	],[
-  // 		"maximum_heat_load",
-  // 		3312125.0171753373
-  // 	]])
+	usageData, 
+	scrollAfterSubmit,
+	setScrollAfterSubmit,
+} : AnalysisHeaderProps) {
+	// Example usageData
+	// new Map([[
+	// 		"estimated_balance_point",
+	// 		61.5
+	// 	],[
+	// 		"other_fuel_usage",
+	// 		0.2857142857142857
+	// 	],[
+	// 		"average_indoor_temperature",
+	// 		67
+	// 	],[
+	// 		"difference_between_ti_and_tbp",
+	// 		5.5
+	// 	],[
+	// 		"design_temperature",
+	// 		1
+	// 	],[
+	// 		"whole_home_heat_loss_rate",
+	// 		48001.81184312083
+	// 	],[
+	// 		"standard_deviation_of_heat_loss_rate",
+	// 		0.08066745182677547
+	// 	],[
+	// 		"average_heat_load",
+	// 		3048115.0520381727
+	// 	],[
+	// 		"maximum_heat_load",
+	// 		3312125.0171753373
+	// 	]])
 
-  // Extract the heat_load_output from usageData
-  const summaryOutputs = usageData?.heat_load_output;
+	// Extract the heat_load_output from usageData
+	const summaryOutputs = usageData?.heat_load_output;
 
-  const totalRecords = usageData?.processed_energy_bills?.length || "-";
+	const totalRecords = usageData?.processed_energy_bills?.length || "-"
 
-  // Calculate the number of billing periods included in Heating calculations
-  const heatingAnalysisTypeRecords = usageData?.processed_energy_bills?.filter(
-    (billingRecord) => billingRecord.analysis_type === 1,
-    // Do wee need this code instead? (billingRecord) => billingRecord.analysis_type !== "NOT_ALLOWED_IN_CALCULATIONS",
-  );
+	// Calculate the number of billing periods included in Heating calculations
+	const heatingAnalysisTypeRecords = usageData?.processed_energy_bills?.filter(
+		(billingRecord) => billingRecord.analysis_type === 1,
+		// Do wee need this code instead? (billingRecord) => billingRecord.analysis_type !== "NOT_ALLOWED_IN_CALCULATIONS",
+	);
 
-  const recordsIncludedByDefault = heatingAnalysisTypeRecords?.filter(
-    (billingRecord) =>
-      billingRecord.default_inclusion === true &&
-      billingRecord.inclusion_override === false,
-  ).length;
+	const recordsIncludedByDefault = heatingAnalysisTypeRecords?.filter(
+		(billingRecord) =>
+		billingRecord.default_inclusion === true &&
+		billingRecord.inclusion_override === false,
+	).length;
 
-  const recordsIncludedByOverride = heatingAnalysisTypeRecords?.filter(
-    (billingRecord) =>
-      billingRecord.default_inclusion === false &&
-      billingRecord.inclusion_override === true,
-  ).length;
+	const recordsIncludedByOverride = heatingAnalysisTypeRecords?.filter(
+		(billingRecord) =>
+		billingRecord.default_inclusion === false &&
+		billingRecord.inclusion_override === true,
+	).length;
 
-  const numRecordsForHeatingCalculations =
-    (recordsIncludedByDefault || 0) + (recordsIncludedByOverride || 0);
+	const numRecordsForHeatingCalculations =
+		(recordsIncludedByDefault || 0) + (recordsIncludedByOverride || 0);
 
-  const targetRef = useRef<HTMLDivElement>(null);
-  /*
+	const targetRef = useRef<HTMLDivElement>(null);
+	/*
 	Scrolls down until the top of the Analysis Header is at the top of 
 	the browser
 
@@ -83,81 +83,80 @@ export function AnalysisHeader({
 	after the user clicks the calculate button, with other, likewise-marked code
 	single.tsx and EnergyUseHistory.tsx.  Do not change it lightly.
 	*/
-  useEffect(() => {
-    if (scrollAfterSubmit) {
-      if (targetRef.current) {
-        targetRef.current.scrollIntoView({ behavior: "smooth" });
-        setScrollAfterSubmit(false);
-      }
-    }
-  }, [scrollAfterSubmit, setScrollAfterSubmit]);
+	useEffect(() => {
+		if (scrollAfterSubmit) {
+			if (targetRef.current) {
+				targetRef.current.scrollIntoView({ behavior: 'smooth' });
+				setScrollAfterSubmit(false);
+			}
+		}
+	}, [scrollAfterSubmit, setScrollAfterSubmit])
+	
+	const titleClassTailwind = 'text-4xl font-bold tracking-wide'
+	const componentMargin = 'mt-10'
 
-  const titleClassTailwind = "text-4xl font-bold tracking-wide";
-  const componentMargin = "mt-10";
+	// Calculate the value
+	const value = summaryOutputs?.standard_deviation_of_heat_loss_rate * 100;
 
-  // Calculate the value
-  const value = summaryOutputs?.standard_deviation_of_heat_loss_rate * 100;
+	// Determine the text color based on the value
+	const textColor = value <= 10 ? 'text-green-400' : 'text-red-500';
 
-  // Determine the text color based on the value
-  const textColor = value <= 10 ? "text-green-400" : "text-red-500";
-
-  return (
-    <div className="section-title mt-12" ref={targetRef}>
-      <div className="mb-4 flex flex-row gap-0.5">
-        <h2 className={`${titleClassTailwind} ${componentMargin}`}>
-          Heat Load Analysis
-        </h2>
-        {/* TODO: add help text here */}
-        <img src={HelpCircle} alt="help text" />
-      </div>
-      <div className="flex flex-row gap-x-4">
-        <div className="basis-1/3">
-          <div className="item-title-small text-xl font-normal text-slate-700">
-            Average Indoor Temperature <br />
-            <div className="item font-bold">
-              {summaryOutputs?.average_indoor_temperature.toFixed(1)} °F
-            </div>
-            <br />
-            Balance Point Temperature
-            <br />
-            <div className="item font-bold">
-              {summaryOutputs?.estimated_balance_point} °F
-            </div>
-            <br />
-          </div>
-        </div>
-        <div className="basis-1/3">
-          <div className="item-title-small text-xl font-normal text-slate-700">
-            Number of Periods Included <br />
-            <div className="item font-bold">
-              {numRecordsForHeatingCalculations} / {totalRecords}
-            </div>
-            <br />
-            Daily Non-heating Usage <br />
-            <div className="item font-bold">
-              {/* Rounding to two decimal places */}
-              {summaryOutputs?.other_fuel_usage?.toFixed(2)} therms
-            </div>
-          </div>
-        </div>
-        <div className="basis-1/3">
-          <div className="item-title-small text-xl font-normal text-slate-700">
-            Standard Deviation of UA <br />
-            <div className={`item font-bold ${textColor}`}>
-              {/* Rounding to two decimal places */}
-              {value?.toFixed(2)} %
-            </div>
-            <br />
-            Whole-home UA
-            <br />
-            <div className="item font-bold">
-              {/* Rounding to zero decimal places */}
-              {summaryOutputs?.whole_home_heat_loss_rate?.toFixed(0)} BTU/h-°F
-            </div>
-            <br />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="section-title mt-12" ref={targetRef}>
+			<div className="flex flex-row gap-0.5 mb-4">
+				<h2 className={`${titleClassTailwind} ${componentMargin}`}>
+					Heat Load Analysis
+				</h2>
+				{/* TODO: add help text here */}
+				<img src={HelpCircle} alt='help text'/>
+			</div>
+			<div className="flex flex-row gap-x-4">
+				<div className="basis-1/3">
+					<div className="item-title-small text-xl text-slate-700 font-normal">
+						Average Indoor Temperature <br />
+						<div className="item font-bold">
+							{summaryOutputs?.average_indoor_temperature.toFixed(1)} °F
+						</div>
+						<br />
+						Balance Point Temperature
+						<br />
+						<div className="item font-bold">
+							{summaryOutputs?.estimated_balance_point} °F
+						</div>
+						<br />
+					</div>
+				</div>
+				<div className="basis-1/3">
+					<div className="item-title-small text-xl text-slate-700 font-normal">
+						Number of Periods Included <br />
+						<div className="item font-bold">{numRecordsForHeatingCalculations} / {totalRecords}</div>
+						<br />
+						Daily Non-heating Usage <br />
+						<div className="item font-bold">
+							{/* Rounding to two decimal places */}
+							{summaryOutputs?.other_fuel_usage?.toFixed(2)} therms
+						</div>
+					</div>
+				</div>
+				<div className="basis-1/3">
+					<div className="item-title-small text-xl text-slate-700 font-normal">
+						Standard Deviation of UA <br />
+						<div className={`item font-bold ${textColor}`}>
+							{/* Rounding to two decimal places */}
+							{(value)?.toFixed(2)}{' '}
+							%
+						</div>
+						<br />
+						Whole-home UA
+						<br />
+						<div className="item font-bold">
+							{/* Rounding to zero decimal places */}
+							{summaryOutputs?.whole_home_heat_loss_rate?.toFixed(0)} BTU/h-°F
+						</div>
+						<br />
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
--- heat-stack/app/root.tsx
+++ heat-stack/app/root.tsx
@@ -1,260 +1,254 @@
-import { OpenImgContextProvider } from "openimg/react";
+import { OpenImgContextProvider } from 'openimg/react'
 import {
-  data,
-  Link,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-  useMatches,
-} from "react-router";
-import { HoneypotProvider } from "remix-utils/honeypot/react";
-import { type Route } from "./+types/root.ts";
-import appleTouchIconAssetUrl from "./assets/favicons/apple-touch-icon.png";
-import faviconAssetUrl from "./assets/favicons/favicon.svg";
-import { GeneralErrorBoundary } from "./components/error-boundary.tsx";
-import { EpicProgress } from "./components/progress-bar.tsx";
-import { SearchBar } from "./components/search-bar.tsx";
-import { useToast } from "./components/toaster.tsx";
-import { Button } from "./components/ui/button.tsx";
-import { href as iconsHref } from "./components/ui/icon.tsx";
-import { EpicToaster } from "./components/ui/sonner.tsx";
-import { UserDropdown } from "./components/user-dropdown.tsx";
-import tailwindStyleSheetUrl from "./styles/tailwind.css?url";
-import { getUserId, logout } from "./utils/auth.server.ts";
-import { ClientHintCheck, getHints } from "./utils/client-hints.tsx";
-import { prisma } from "./utils/db.server.ts";
-import { getEnv } from "./utils/env.server.ts";
-import { pipeHeaders } from "./utils/headers.server.ts";
-import { honeypot } from "./utils/honeypot.server.ts";
-import { combineHeaders, getDomainUrl, getImgSrc } from "./utils/misc.tsx";
-import { useNonce } from "./utils/nonce-provider.ts";
-import { makeTimings, time } from "./utils/timing.server.ts";
-import { getToast } from "./utils/toast.server.ts";
-import { useOptionalUser } from "./utils/user.ts";
+	data,
+	Link,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLoaderData,
+	useMatches,
+} from 'react-router'
+import { HoneypotProvider } from 'remix-utils/honeypot/react'
+import { type Route } from './+types/root.ts'
+import appleTouchIconAssetUrl from './assets/favicons/apple-touch-icon.png'
+import faviconAssetUrl from './assets/favicons/favicon.svg'
+import { GeneralErrorBoundary } from './components/error-boundary.tsx'
+import { EpicProgress } from './components/progress-bar.tsx'
+import { SearchBar } from './components/search-bar.tsx'
+import { useToast } from './components/toaster.tsx'
+import { Button } from './components/ui/button.tsx'
+import { href as iconsHref } from './components/ui/icon.tsx'
+import { EpicToaster } from './components/ui/sonner.tsx'
+import { UserDropdown } from './components/user-dropdown.tsx'
+import tailwindStyleSheetUrl from './styles/tailwind.css?url'
+import { getUserId, logout } from './utils/auth.server.ts'
+import { ClientHintCheck, getHints } from './utils/client-hints.tsx'
+import { prisma } from './utils/db.server.ts'
+import { getEnv } from './utils/env.server.ts'
+import { pipeHeaders } from './utils/headers.server.ts'
+import { honeypot } from './utils/honeypot.server.ts'
+import { combineHeaders, getDomainUrl, getImgSrc } from './utils/misc.tsx'
+import { useNonce } from './utils/nonce-provider.ts'
+import { makeTimings, time } from './utils/timing.server.ts'
+import { getToast } from './utils/toast.server.ts'
+import { useOptionalUser } from './utils/user.ts'
 
 export const links: Route.LinksFunction = () => {
-  return [
-    // Preload svg sprite as a resource to avoid render blocking
-    { rel: "preload", href: iconsHref, as: "image" },
-    {
-      rel: "icon",
-      href: "/favicon.ico",
-      sizes: "48x48",
-    },
-    { rel: "icon", type: "image/svg+xml", href: faviconAssetUrl },
-    { rel: "apple-touch-icon", href: appleTouchIconAssetUrl },
-    {
-      rel: "manifest",
-      href: "/site.webmanifest",
-      crossOrigin: "use-credentials",
-    } as const, // necessary to make typescript happy
-    { rel: "stylesheet", href: tailwindStyleSheetUrl },
-  ].filter(Boolean);
-};
-
-export const meta: Route.MetaFunction = ({ data }) => {
-  return [
-    { title: data ? "HEAT Analysis Tool" : "Error | HEAT Analysis Tool" },
-    { name: "description", content: `Your own captain's log` },
-  ];
-};
-
-export async function loader({ request }: Route.LoaderArgs) {
-  const timings = makeTimings("root loader");
-  const userId = await time(() => getUserId(request), {
-    timings,
-    type: "getUserId",
-    desc: "getUserId in root",
-  });
-
-  const user = userId
-    ? await time(
-        () =>
-          prisma.user.findUnique({
-            select: {
-              id: true,
-              name: true,
-              username: true,
-              image: { select: { objectKey: true } },
-              roles: {
-                select: {
-                  name: true,
-                  permissions: {
-                    select: { entity: true, action: true, access: true },
-                  },
-                },
-              },
-            },
-            where: { id: userId },
-          }),
-        { timings, type: "find user", desc: "find user in root" },
-      )
-    : null;
-  if (userId && !user) {
-    console.info("something weird happened");
-    // something weird happened... The user is authenticated but we can't find
-    // them in the database. Maybe they were deleted? Let's log them out.
-    await logout({ request, redirectTo: "/" });
-  }
-  const { toast, headers: toastHeaders } = await getToast(request);
-  const honeyProps = await honeypot.getInputProps();
-
-  return data(
-    {
-      user,
-      requestInfo: {
-        hints: getHints(request),
-        origin: getDomainUrl(request),
-        path: new URL(request.url).pathname,
-        userPrefs: {
-          // theme: getTheme(request),
-        },
-      },
-      ENV: getEnv(),
-      toast,
-      honeyProps,
-    },
-    {
-      headers: combineHeaders(
-        { "Server-Timing": timings.toString() },
-        toastHeaders,
-      ),
-    },
-  );
+	return [
+		// Preload svg sprite as a resource to avoid render blocking
+		{ rel: 'preload', href: iconsHref, as: 'image' },
+		{
+			rel: 'icon',
+			href: '/favicon.ico',
+			sizes: '48x48',
+		},
+		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
+		{ rel: 'apple-touch-icon', href: appleTouchIconAssetUrl },
+		{
+			rel: 'manifest',
+			href: '/site.webmanifest',
+			crossOrigin: 'use-credentials',
+		} as const, // necessary to make typescript happy
+		{ rel: 'stylesheet', href: tailwindStyleSheetUrl },
+	].filter(Boolean)
 }
 
-export const headers: Route.HeadersFunction = pipeHeaders;
+export const meta: Route.MetaFunction = ({ data }) => {
+	return [
+		{ title: data ? 'HEAT Analysis Tool' : 'Error | HEAT Analysis Tool' },
+		{ name: 'description', content: `Your own captain's log` },
+	]
+}
+
+export async function loader({ request }: Route.LoaderArgs) {
+	const timings = makeTimings('root loader')
+	const userId = await time(() => getUserId(request), {
+		timings,
+		type: 'getUserId',
+		desc: 'getUserId in root',
+	})
+
+	const user = userId
+		? await time(
+			() =>
+				prisma.user.findUnique({
+					select: {
+						id: true,
+						name: true,
+						username: true,
+						image: { select: { objectKey: true } },
+						roles: {
+							select: {
+								name: true,
+								permissions: {
+									select: { entity: true, action: true, access: true },
+								},
+							},
+						},
+					},
+					where: { id: userId },
+				}),
+			{ timings, type: 'find user', desc: 'find user in root' },
+		)
+		: null
+	if (userId && !user) {
+		console.info('something weird happened')
+		// something weird happened... The user is authenticated but we can't find
+		// them in the database. Maybe they were deleted? Let's log them out.
+		await logout({ request, redirectTo: '/' })
+	}
+	const { toast, headers: toastHeaders } = await getToast(request)
+	const honeyProps = await honeypot.getInputProps()
+
+	return data(
+		{
+			user,
+			requestInfo: {
+				hints: getHints(request),
+				origin: getDomainUrl(request),
+				path: new URL(request.url).pathname,
+				userPrefs: {
+					// theme: getTheme(request),
+				},
+			},
+			ENV: getEnv(),
+			toast,
+			honeyProps,
+		},
+		{
+			headers: combineHeaders(
+				{ 'Server-Timing': timings.toString() },
+				toastHeaders,
+			),
+		},
+	)
+}
+
+export const headers: Route.HeadersFunction = pipeHeaders
 
 function Document({
-  children,
-  nonce,
-  env = {},
+	children,
+	nonce,
+	env = {},
 }: {
-  children: React.ReactNode;
-  nonce: string;
-  env?: Record<string, string | undefined>;
+	children: React.ReactNode
+	nonce: string
+	env?: Record<string, string | undefined>
 }) {
-  const allowIndexing = ENV.ALLOW_INDEXING !== "false";
-  return (
-    <html lang="en" className={`h-full`}>
-      <head>
-        <ClientHintCheck nonce={nonce} />
-        <Meta />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        {allowIndexing ? null : (
-          <meta name="robots" content="noindex, nofollow" />
-        )}
-        <Links />
-      </head>
-      <body className="bg-background text-foreground">
-        <div className="container items-center justify-between gap-4 md:h-24 md:flex-row">
-          {children}
-        </div>
-        <script
-          nonce={nonce}
-          dangerouslySetInnerHTML={{
-            __html: `window.ENV = ${JSON.stringify(env)}`,
-          }}
-        />
-        <ScrollRestoration nonce={nonce} />
-        <Scripts nonce={nonce} />
-      </body>
-    </html>
-  );
+	const allowIndexing = ENV.ALLOW_INDEXING !== 'false'
+	return (
+		<html lang="en" className={`h-full`}>
+			<head>
+				<ClientHintCheck nonce={nonce} />
+				<Meta />
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				{allowIndexing ? null : (
+					<meta name="robots" content="noindex, nofollow" />
+				)}
+				<Links />
+			</head>
+			<body className="bg-background text-foreground">
+				<div className="container items-center justify-between gap-4 md:h-24 md:flex-row">
+
+					{children}
+				</div>
+				<script
+					nonce={nonce}
+					dangerouslySetInnerHTML={{
+						__html: `window.ENV = ${JSON.stringify(env)}`,
+					}}
+				/>
+				<ScrollRestoration nonce={nonce} />
+				<Scripts nonce={nonce} />
+			</body>
+		</html>
+	)
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  // if there was an error running the loader, data could be missing
-  const data = useLoaderData<typeof loader | null>();
-  const nonce = useNonce();
-  return (
-    <Document nonce={nonce} env={data?.ENV}>
-      {children}
-    </Document>
-  );
+	// if there was an error running the loader, data could be missing
+	const data = useLoaderData<typeof loader | null>()
+	const nonce = useNonce()
+	return (
+		<Document nonce={nonce} env={data?.ENV}>
+			{children}
+		</Document>
+	)
 }
 
 function App() {
-  const data = useLoaderData<typeof loader>();
-  const user = useOptionalUser();
-  const matches = useMatches();
-  const isOnSearchPage = matches.find((m) => m.id === "routes/users+/index");
-  const isOnHomePage = matches.find((match) => {
-    let data = {} as any; // Typescript is freaking out about m.data.requestInfo
-    if (match && match.data) {
-      data = match.data;
-    }
-    if (data.requestInfo && data.requestInfo.path) {
-      return data.requestInfo.path === "/";
-    }
-  });
-  const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />;
-  useToast(data.toast);
-  console.log("Home page rendered.");
+	const data = useLoaderData<typeof loader>()
+	const user = useOptionalUser()
+	const matches = useMatches()
+	const isOnSearchPage = matches.find((m) => m.id === 'routes/users+/index')
+	const isOnHomePage = matches.find((match) => {
+		let data = {} as any;  // Typescript is freaking out about m.data.requestInfo
+		if (match && match.data) { data = match.data; }
+		if (data.requestInfo && data.requestInfo.path) {
+			return data.requestInfo.path === '/'
+		}
+	})
+	const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
+	useToast(data.toast)
+	console.log("Home page rendered.")
 
-  return (
-    <OpenImgContextProvider
-      optimizerEndpoint="/resources/images"
-      getSrc={getImgSrc}
-    >
-      <div className="flex min-h-screen flex-col justify-between">
-        <header className="container px-0 py-6">
-          {!isOnHomePage ? (
-            <Link to="/">
-              <section
-                className="bg-gradient-to-r from-emerald-600 to-teal-500 py-5 text-white"
-                style={{ borderRadius: "10px" }}
-              >
-                <div className="mx-auto max-w-5xl text-center">
-                  <h1 className="text-4xl font-bold md:text-4xl">
-                    Home Energy Analysis Tool (HEAT)
-                  </h1>
-                </div>
-              </section>
-            </Link>
-          ) : null}
+	return (
+		<OpenImgContextProvider
+			optimizerEndpoint="/resources/images"
+			getSrc={getImgSrc}
+		>
+			<div className="flex min-h-screen flex-col justify-between">
+				<header className="container py-6 px-0">
+					{!isOnHomePage ? <Link to="/">
+						<section className="bg-gradient-to-r from-emerald-600 to-teal-500 py-5 text-white" style={{ borderRadius:'10px' }}>
+							<div className="max-w-5xl mx-auto text-center">
+								<h1 className="text-4xl md:text-4xl font-bold">Home Energy Analysis Tool (HEAT)</h1>
+							</div>
+						</section>
+					</Link> : null}
 
-          <nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-            <div className="ml-auto hidden max-w-sm flex-1">{searchBar}</div>
-            <div className="flex items-center gap-10">
-              {user ? (
-                <UserDropdown />
-              ) : (
-                <Button asChild variant="default" size="lg">
-                  {/* <Link to="/login">Log In</Link> */}
-                </Button>
-              )}
-            </div>
-            <div className="hidden w-full sm:hidden">{searchBar}</div>
-          </nav>
-        </header>
+					<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
+						<div className="ml-auto hidden max-w-sm flex-1">
+							{searchBar}
+						</div>
+						<div className="flex items-center gap-10">
+							{user ? (
+								<UserDropdown />
+							) : (
+								<Button asChild variant="default" size="lg">
+									{/* <Link to="/login">Log In</Link> */}
+								</Button>
+							)}
+						</div>
+						<div className="hidden w-full sm:hidden">{searchBar}</div>
+					</nav>
+				</header>
 
-        <div className="flex flex-1 flex-col">
-          <Outlet />
-          <div className="container flex justify-between pb-5">&nbsp;</div>
-        </div>
-      </div>
-      <EpicToaster closeButton position="top-center" />
-      <EpicProgress />
-    </OpenImgContextProvider>
-  );
+				<div className="flex flex-1 flex-col">
+					<Outlet />
+					<div className="container flex justify-between pb-5">&nbsp;</div>
+				</div>
+			</div>
+			<EpicToaster closeButton position="top-center" />
+			<EpicProgress />
+		</OpenImgContextProvider>
+	)
 }
 
 function AppWithProviders() {
-  const data = useLoaderData<typeof loader>();
-  return (
-    <HoneypotProvider {...data.honeyProps}>
-      <App />
-    </HoneypotProvider>
-  );
+	const data = useLoaderData<typeof loader>()
+	return (
+		<HoneypotProvider {...data.honeyProps}>
+			<App />
+		</HoneypotProvider>
+	)
 }
 
-export default AppWithProviders;
+export default AppWithProviders
 
 // this is a last resort error boundary. There's not much useful information we
 // can offer at this level.
-export const ErrorBoundary = GeneralErrorBoundary;
+export const ErrorBoundary = GeneralErrorBoundary
