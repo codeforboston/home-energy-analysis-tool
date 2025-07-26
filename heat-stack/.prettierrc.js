48458675cdba5b94e9121433a8f4c231a0c3c8fb
AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>
2025-07-26 12:52:31 -0400

    Migrated to BiomeJS
    Co-authored-by: plocket <plocket@users.noreply.github.com>

--- heat-stack/.prettierrc.js
+++ heat-stack/.prettierrc.js
@@ -1,6 +1,10 @@
+import { has } from './utils.js'
+
+const hasTailwind = has('tailwindcss')
+
 /** @type {import("prettier").Options} */
-export default {
-	arrowParens: 'avoid',
+export const config = {
+	arrowParens: 'always',
 	bracketSameLine: false,
 	bracketSpacing: true,
 	embeddedLanguageFormatting: 'auto',
@@ -19,12 +23,32 @@ export default {
 	trailingComma: 'all',
 	useTabs: true,
 	overrides: [
+		// formatting the package.json with anything other than spaces will cause
+		// issues when running install...
 		{
-			files: ['**/*.json'],
+			files: ['**/package.json'],
 			options: {
 				useTabs: false,
 			},
 		},
+		{
+			files: ['**/*.mdx'],
+			options: {
+				// This stinks, if you don't do this, then an inline component on the
+				// end of the line will end up wrapping, then the next save Prettier
+				// will add an extra line break. Super annoying and probably a bug in
+				// Prettier, but until it's fixed, this is the best we can do.
+				proseWrap: 'preserve',
+				htmlWhitespaceSensitivity: 'ignore',
+			},
+		},
 	],
-	plugins: ['prettier-plugin-tailwindcss'],
+	...(hasTailwind && {
+		plugins: ['prettier-plugin-tailwindcss'],
+		tailwindAttributes: ['class', 'className', 'ngClass', '.*[cC]lassName'],
+		tailwindFunctions: ['clsx', 'cn', 'cva'],
+	}),
 }
+
+// this is for backward compatibility
+export default config
