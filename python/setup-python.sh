3869d88c22944ec3b7ed2339b487fc9396caa15a
AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>
2025-07-29 21:31:51 -0400

    Switched to uv (#520)
    * Switched to uv
    
    * Changed rules engine setup action to comply with uv
    
    * Removed linting the dist directory
    
    * Added development dependencies to virtual environment for workflow
    
    * Added development dependency installation
    
    * Excluded build folder from mypy workflow
    
    * Removed unnecessary README lines
    
    Co-authored-by: Ethan-Strominger <32078396+ethanstrominger@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ethan-Strominger <32078396+ethanstrominger@users.noreply.github.com>

--- python/setup-python.sh
+++ python/setup-python.sh
@@ -3,43 +3,20 @@
 # Trap errors and print a message
 trap 'echo "An error occurred"; set +x' ERR
 
+# Install dependencies, including rules-engine in editable mode
+uv add --dev .
+
+# Install development dependencies
+uv pip install ".[dev]"
+
 # Detect the operating system
 OS=$(uname)
 
-# Set the PYTHON_CMD environment variable based on the operating system
-if [[ "$OS" == "Linux" ]] || [[ "$OS" == "Darwin" ]]; then
-    # Set the PYTHON_CMD environment variable for Linux or macOS
-    export PYTHON_CMD="python3.12"
-elif [[ "$OS" == *"MINGW"* ]] || [[ "$OS" == *"CYGWIN"* ]]; then
-    # Set the PYTHON_CMD environment variable for Windows (using Git Bash or WSL)
-    export PYTHON_CMD="py -3.12"
-else
-    echo "Unsupported operating system: $OS"
-    exit 1
-fi
-
-# Print the PYTHON_CMD for verification
-echo "PYTHON_CMD is set to: $PYTHON_CMD"
-
-echo "Using Python command: $PYTHON_CMD"
-
-# Source the check-version.sh script
-source check-version.sh || { echo "Error: check-version.sh failed"; exit 1; }
-
-# Create a virtual environment
-$PYTHON_CMD -m venv venv
-
 # Activate the virtual environment
 case "$OSTYPE" in
-  msys*) source venv/Scripts/activate;;
-  *) source venv/bin/activate;;
+  msys*) source .venv/Scripts/activate;;
+  *) source .venv/bin/activate;;
 esac
-
-# Install dependencies
-pip install -e .
-pip install -r requirements-dev.txt
-pip install --upgrade pip
-pre-commit install
 
 # End of script
 set +x
