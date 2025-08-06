15501d2ca501abbd325a7543373d789885d4c666
Thad Kerosky <contact@thadk.net>
2025-08-05 20:41:00 -0400

    Chore/509/regression checking inclusion default has no effect rebase (#535)
    * Rebase from main
    
    * Rebase from main
    
    * Debugging regression
    
    * Finalize fix
    
    * Remove console.log
    
    * give uv to playwright gha
    
    * Fix up test for csv and checkbox click, gitignore
    
    Co-authored-by: AdamFinkle <AdamFinkle@users.noreply.github.com>
    Co-authored-by: nikulabs <nikulabs@users.noreply.github.com>
    Co-authored-by: dwindleduck <dwindleduck@users.noreply.github.com>
    
    * Fix up test for csv and checkbox click, gitignore
    
    Co-authored-by: AdamFinkle <AdamFinkle@users.noreply.github.com>
    Co-authored-by: nikulabs <nikulabs@users.noreply.github.com>
    Co-authored-by: dwindleduck <dwindleduck@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>
    Co-authored-by: AdamFinkle <AdamFinkle@users.noreply.github.com>
    Co-authored-by: nikulabs <nikulabs@users.noreply.github.com>
    Co-authored-by: dwindleduck <dwindleduck@users.noreply.github.com>

--- heat-stack/tests/e2e/energy-analysis.test.ts
+++ heat-stack/tests/e2e/energy-analysis.test.ts
@@ -1,7 +1,11 @@
-import { faker } from '@faker-js/faker'
-import { type NoteImage, type Note } from '@prisma/client'
-import { prisma } from '#app/utils/db.server.ts'
 import { expect, test } from '#tests/playwright-utils.ts'
+import { type Page as PlaywrightPage } from 'playwright'
+
+const getAnalysisHeaderTextContent = async (page: PlaywrightPage) => {
+	return await page.getByTestId('analysis-header').textContent()
+}
+
+
 test.setTimeout(120000)
 test('Logged out user can upload CSV, toggle table row checkbox, expecting analysis header to adjust.', async ({ page, login }) => {
 	// Visit the root
@@ -9,7 +13,6 @@ test('Logged out user can upload CSV, toggle table row checkbox, expecting analy
 
 	// click the "demo data" link
 	await page.getByText('Get Started (with Demo Data)').click()
-	await expect(page).toHaveURL(`/single?dev=true`)
 
 	await page
 		.getByLabel("Upload your energy billing company's bill.")
@@ -17,21 +20,33 @@ test('Logged out user can upload CSV, toggle table row checkbox, expecting analy
 		.setInputFiles('tests/fixtures/csv/green_button_gas_bill_quateman_for_test.csv')
 
 	await page.locator('button[name="intent"][value="upload"]').click()
-	await page.waitForTimeout(15000) 
+
+	// Waits for the URL, continues as soon as the URL appears
+	// and times out if 15 seconds have passed without the URL appearing
+	await page.waitForURL('/single', { timeout: 15_000 });
 	// await page.screenshot({ 
 	// 	path: 'full-page.png',
 	// 	fullPage: true 
 	// 	})
-	await expect(page).toHaveURL(`/single`)
 
 	//Toggle table row checkbox, expecting "Analysis Header" text to change.
 	// save the analysis header text before checkbox click
-	const analysisHeader_beforeClick = page.getByTestId("analysis-header").toString()
+	const tableHeaderContentBeforeClick = await getAnalysisHeaderTextContent(page);
+		
 	// click the first checkbox and wait
-	await page.locator('[role="checkbox"]').first().click();
-	await page.waitForTimeout(15000) 
+	const checkbox = page.locator('[role="checkbox"]').first();
+
+	// Assert the un-checked state
+	await expect(checkbox).not.toBeChecked();
+
+	// Click the checkbox
+	await checkbox.click();
+
+	// Assert the checked state
+	await expect(checkbox).toBeChecked();
 	// save the analysis header text after checkbox click and wait
-	const analysisHeader_afterClick = page.getByTestId("analysis-header").toString()
+	const tableHeaderContentAfterClick = await getAnalysisHeaderTextContent(page);
+	
 	// expect not equal
-	expect(analysisHeader_afterClick).not.toEqual(analysisHeader_beforeClick)
+	expect(tableHeaderContentAfterClick).not.toEqual(tableHeaderContentBeforeClick);
 })
