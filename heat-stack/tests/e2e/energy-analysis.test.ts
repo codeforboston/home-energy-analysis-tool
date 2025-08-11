047966efa5d0a4d6de9d110e4b9b392f9f27a0c3
Wil Interiano <29824849+interianow@users.noreply.github.com>
2025-08-11 12:36:37 -0400

    chore: applied prettier formatting, removed unused arg, fixed import order

--- heat-stack/tests/e2e/energy-analysis.test.ts
+++ heat-stack/tests/e2e/energy-analysis.test.ts
@@ -1,13 +1,14 @@
-import { expect, test } from '#tests/playwright-utils.ts'
 import { type Page as PlaywrightPage } from 'playwright'
+import { expect, test } from '#tests/playwright-utils.ts'
 
 const getAnalysisHeaderTextContent = async (page: PlaywrightPage) => {
 	return await page.getByTestId('analysis-header').textContent()
 }
 
-
 test.setTimeout(120000)
-test('Logged out user can upload CSV, toggle table row checkbox, expecting analysis header to adjust.', async ({ page, login }) => {
+test('Logged out user can upload CSV, toggle table row checkbox, expecting analysis header to adjust.', async ({
+	page,
+}) => {
 	// Visit the root
 	await page.goto('/')
 
@@ -17,36 +18,40 @@ test('Logged out user can upload CSV, toggle table row checkbox, expecting analy
 	await page
 		.getByLabel("Upload your energy billing company's bill.")
 		.nth(0)
-		.setInputFiles('tests/fixtures/csv/green_button_gas_bill_quateman_for_test.csv')
+		.setInputFiles(
+			'tests/fixtures/csv/green_button_gas_bill_quateman_for_test.csv',
+		)
 
 	await page.locator('button[name="intent"][value="upload"]').click()
 
 	// Waits for the URL, continues as soon as the URL appears
 	// and times out if 15 seconds have passed without the URL appearing
-	await page.waitForURL('/single', { timeout: 15_000 });
-	// await page.screenshot({ 
+	await page.waitForURL('/single', { timeout: 15_000 })
+	// await page.screenshot({
 	// 	path: 'full-page.png',
-	// 	fullPage: true 
+	// 	fullPage: true
 	// 	})
 
 	//Toggle table row checkbox, expecting "Analysis Header" text to change.
 	// save the analysis header text before checkbox click
-	const tableHeaderContentBeforeClick = await getAnalysisHeaderTextContent(page);
-		
+	const tableHeaderContentBeforeClick = await getAnalysisHeaderTextContent(page)
+
 	// click the first checkbox and wait
-	const checkbox = page.locator('[role="checkbox"]').first();
+	const checkbox = page.locator('[role="checkbox"]').first()
 
 	// Assert the un-checked state
-	await expect(checkbox).not.toBeChecked();
+	await expect(checkbox).not.toBeChecked()
 
 	// Click the checkbox
-	await checkbox.click();
+	await checkbox.click()
 
 	// Assert the checked state
-	await expect(checkbox).toBeChecked();
+	await expect(checkbox).toBeChecked()
 	// save the analysis header text after checkbox click and wait
-	const tableHeaderContentAfterClick = await getAnalysisHeaderTextContent(page);
-	
+	const tableHeaderContentAfterClick = await getAnalysisHeaderTextContent(page)
+
 	// expect not equal
-	expect(tableHeaderContentAfterClick).not.toEqual(tableHeaderContentBeforeClick);
+	expect(tableHeaderContentAfterClick).not.toEqual(
+		tableHeaderContentBeforeClick,
+	)
 })
