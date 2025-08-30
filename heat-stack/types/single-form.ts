0c5e80fe2d20ac85f04c2453d7f6a625fe5d3909
Wil Interiano <29824849+interianow@users.noreply.github.com>
2025-08-30 17:02:51 -0400

    fix: usagedata to recalculate after a successful submit and centralized all usage calculations in one place

---
+++ heat-stack/types/single-form.ts
@@ -0,0 +1,26 @@
+import { type z } from 'zod'
+import { CaseSchema, HomeSchema, LocationSchema, UploadEnergyUseFileSchema } from "."
+
+/** TODO: Use url param "dev" to set defaults */
+/** Modeled off the conform example at
+ *     https://github.com/epicweb-dev/web-forms/blob/b69e441f5577b91e7df116eba415d4714daacb9d/exercises/03.schema-validation/03.solution.conform-form/app/routes/users%2B/%24username_%2B/notes.%24noteId_.edit.tsx#L48 */
+
+const HomeFormSchema = HomeSchema.pick({ living_area: true })
+    .and(LocationSchema.pick({ street_address: true, town: true, state: true }))
+    .and(CaseSchema.pick({ name: true }))
+
+const CurrentHeatingSystemSchema = HomeSchema.pick({
+    fuel_type: true,
+    heating_system_efficiency: true,
+    design_temperature_override: true,
+    thermostat_set_point: true,
+    setback_temperature: true,
+    setback_hours_per_day: true,
+})
+
+
+export const Schema = UploadEnergyUseFileSchema.and(HomeFormSchema.and(
+    CurrentHeatingSystemSchema)
+) /* .and(HeatLoadAnalysisZod.pick({design_temperature: true})) */
+
+export type SchemaZodFromFormType = z.infer<typeof Schema>
