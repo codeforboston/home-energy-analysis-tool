3f8cc02896a3afd1c4c1981ee9bf746e92d20105
Ethan Strominger <ethanstrominger2@gmail.com>
2025-11-19 16:09:11 -0500

    Logic to keep track of user.id and secure it at route.ts

---
+++ heat-stack/app/routes/cases+/route.ts
@@ -0,0 +1,7 @@
+import  { type LoaderFunctionArgs } from '@remix-run/node'
+import { requireUserId } from '#app/utils/auth.server.ts'
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  await requireUserId(request)
+  return null
+}
