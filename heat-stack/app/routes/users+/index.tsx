a8bec80d2caff851e642b224d071eabeafe1adf9
Ethan Strominger <ethanstrominger2@gmail.com>
2025-11-26 13:37:31 -0500

    Fix index order

--- heat-stack/app/routes/users+/index.tsx
+++ heat-stack/app/routes/users+/index.tsx
@@ -1,6 +1,4 @@
 import { searchUsers } from '@prisma/client/sql'
-import { ACCESS_DENIED_MESSAGE } from '../../constants/error-messages'
-import { useOptionalUser } from '#app/utils/user.ts'
 import { Img } from 'openimg/react'
 import { redirect, Link } from 'react-router'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
@@ -8,6 +6,9 @@ import { ErrorList } from '#app/components/forms.tsx'
 import { SearchBar } from '#app/components/search-bar.tsx'
 import { prisma } from '#app/utils/db.server.ts'
 import { cn, getUserImgSrc, useDelayedIsPending } from '#app/utils/misc.tsx'
+import { useOptionalUser } from '#app/utils/user.ts'
+import { ACCESS_DENIED_MESSAGE } from '../../constants/error-messages'
+// ...existing code...
 import { type Route } from './+types/index.ts'
 
 export async function loader({ request }: Route.LoaderArgs) {
