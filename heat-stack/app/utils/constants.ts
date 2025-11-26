5513b1dd961870ae48651127a5b9103b9c4fce70
Ethan Strominger <ethanstrominger2@gmail.com>
2025-11-26 14:08:42 -0500

    Fix formatting

--- heat-stack/app/utils/constants.ts
+++ heat-stack/app/utils/constants.ts
@@ -3,9 +3,9 @@
 
 export const EMAIL_FROM = 'no-reply@heat.heatsmartalliance.org'
 
-// CACHE_DATABASE_FILE: from env, or default if not PROD
-export const CACHE_DATABASE_FILE = (() => {
-	const envValue = process.env.CACHE_DATABASE_FILE
+// CACHE_DATABASE_PATH: from env, or default if not PROD
+export const CACHE_DATABASE_PATH = (() => {
+	const envValue = process.env.CACHE_DATABASE_PATH
 	if (envValue) return envValue
 	const nodeEnv = process.env.NODE_ENV || ''
 	if (!nodeEnv.startsWith('PROD')) {
