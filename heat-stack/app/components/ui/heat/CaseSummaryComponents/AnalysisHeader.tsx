38dc559d5c6f16c81c80a4571806cb83c4e5f489
ZaimaGull <ZaimaGull@Gmail.com>
2025-12-16 12:39:10 -0500

    Removed help button on heat load analysis

--- heat-stack/app/components/ui/heat/CaseSummaryComponents/AnalysisHeader.tsx
+++ heat-stack/app/components/ui/heat/CaseSummaryComponents/AnalysisHeader.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react'
 import { type UsageDataSchema } from '#/types/types.ts'
-import { HelpButton } from '../../HelpButton'
 
 interface AnalysisHeaderProps {
 	usageData: UsageDataSchema
@@ -116,8 +115,6 @@ export function AnalysisHeader({
 				<h2 className={`${titleClassTailwind} ${componentMargin}`}>
 					Heat Load Analysis
 				</h2>
-				{/* TODO: add help text here */}
-				<HelpButton keyName="heat_load_analysis.help" />
 			</div>
 			<div
 				data-pw="analysis-header"
