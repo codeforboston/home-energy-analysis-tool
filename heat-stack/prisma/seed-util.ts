de7d25f132a143cbd1621e372f9276ee191a0802
Ethan Strominger <ethanstrominger2@gmail.com>
2025-12-26 19:39:24 -0500

    Remove unneeded file

--- heat-stack/prisma/seed-util.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcryptjs'
-import { faker } from '@faker-js/faker'
-
-export function createPassword(password: string = faker.internet.password()) {
-	return {
-		hash: bcrypt.hashSync(password, 10),
-	}
-}
