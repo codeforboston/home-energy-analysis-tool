df5cb63b896e3a460aef13cf5021373d264efe55
ZaimaGull <ZaimaGull@Gmail.com>
2026-01-20 23:17:49 -0500

    Displayed the Privacy Policy. (#657)
    * Displayed the Privacy Policy.
    
    Closes #631
    
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    
    * Tweak style
    
    * npm run format
    
    ---------
    
    Co-authored-by: plocket <plocket@users.noreply.github.com>
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- heat-stack/app/routes/_marketing+/privacy.tsx
+++ heat-stack/app/routes/_marketing+/privacy.tsx
@@ -1,3 +1,45 @@
 export default function PrivacyRoute() {
-	return <div>Privacy</div>
+	return (
+		<section id="privacy" className="bg-white px-4 py-16">
+			<div className="prose mx-auto max-w-5xl list-disc">
+				<h2 className="mb-12 text-center text-4xl font-bold text-gray-800">
+					Privacy Policy
+				</h2>
+				<p className="mb-4 text-gray-600">
+					The Home Energy Analysis Tool (HEAT) is a project of the HeatSmart
+					Alliance. HEAT retains information that users enter about themselves
+					or others that may not be publicly available, such as an individual’s
+					name in connection with an email address and/or mobile phone number.
+					We consider the aggregation of these data to be personal information.
+					The Alliance strives to use personal information consistent with the
+					intent of the provider (i.e., the individual whose information we
+					possess). As such, the Alliance:
+				</p>
+				<ul className="marker:text-gray-900">
+					<li>
+						Collects personal information for the purposes of a) providing and
+						communicating about the services the individual is requesting, and
+						b) gathering information to improve our services{' '}
+					</li>
+					<li>
+						Does not, in general, share personal information with individuals or
+						organizations outside the Alliance unless the provider explicitly
+						authorizes such sharing. Exception: The Alliance may share personal
+						information provided in a coaching request, or other requests for
+						assistance, with another organization that is known to the Alliance
+						to provide such services
+					</li>
+					<li>
+						Limits access to personal information to the user who entered the
+						information and Alliance Members who have specific responsibilities
+						requiring such access
+					</li>
+					<li>
+						Provides individuals with access to this privacy policy whenever we
+						request personal information.
+					</li>
+				</ul>
+			</div>
+		</section>
+	)
 }
