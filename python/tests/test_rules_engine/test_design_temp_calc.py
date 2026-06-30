7881a9bfee9d8c771d0fc3087bd0df884ae0b74b
Nolan Schroeder <90803031+nolanschroeder@users.noreply.github.com>
2026-06-30 12:47:56 -0400

    Apply black formatting

--- python/tests/test_rules_engine/test_design_temp_calc.py
+++ python/tests/test_rules_engine/test_design_temp_calc.py
@@ -1,21 +1,22 @@
 from datetime import date
-from rules_engine.design_temp_calc import calculate_design_temperatures, get_date_range
+from rules_engine.helpers import calculate_design_temperature, get_date_range
 from pytest import approx
 
+LAT = 42.29244555
+LON = -70.98631661
+
+
 def test_design_temperature_calculator():
-    """Validate the design temperature calculator with a given lat and lon representing a home address"""
-    
-    # input: home address (lat, lon), start_date, end_date
-    # output: design_temp = float
-    LAT = 42.29244555
-    LON = -70.98631661
+    """
+    Validate the design temperature calculator with a given lat and lon representing a home address
+    Input: home address (lat, lon), start_date, end_date
+    Output: design_temp = float
+    """
 
     start_date, end_date = get_date_range(30)
 
-    design_temp, elapsed = calculate_design_temperatures(LAT, LON, start_date=start_date, end_date=end_date)
+    design_temp, elapsed = calculate_design_temperature(
+        LAT, LON, start_date=start_date, end_date=end_date
+    )
 
-    # check for the specified value
     assert design_temp == approx(12.193, abs=0.1)
-
-
-
