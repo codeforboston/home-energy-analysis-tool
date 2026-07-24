f9d731fdbfb4311e9f91aecaa845c834976bf4dc
Marcus Doucette <Marz3587@gmail.com>
2026-07-24 19:38:54 -0400

    fix alignment and text

--- heat-stack/app/components/ui/CustomFileUpload.tsx
+++ heat-stack/app/components/ui/CustomFileUpload.tsx
@@ -36,7 +36,12 @@ export function CustomFileUpload({ name }: { name: string }) {
 
 				{/* Display filename */}
 				<span className="text-sm">{fileName}</span>
-
+				<a
+					className="ml-3 inline-flex items-center gap-1 rounded-md border border-transparent bg-secondary px-2.5 py-0.5 text-xs font-semibold text-secondary-foreground transition-colors hover:bg-secondary/80 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
+					href="https://heatsmartalliance.org/wp-content/uploads/2026/07/HEAT-demo-gas-usage.csv"
+				>
+					Get demo CSV file
+				</a>
 				{/* Help icon immediately after filename */}
 				<HelpButton keyName="download.help" />
 			</div>
